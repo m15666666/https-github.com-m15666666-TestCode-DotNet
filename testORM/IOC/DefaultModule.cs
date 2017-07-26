dc63ddbcab81d7c768f91e0a1f720d88498a9cbe
GYC <GYC@BG001991>
2017-07-26 16:51:43 +0800

    增加mysql+ef6的代码。

--- testORM/IOC/DefaultModule.cs
+++ testORM/IOC/DefaultModule.cs
@@ -17,9 +17,14 @@ namespace testORM.IOC
         //private const string connectionString = "data source=10.3.2.188;initial catalog=testORM;persist security info=True;user id=sa;password=#db877350;MultipleActiveResultSets=True;App=EntityFramework";
 
         // postgresql
-        private const string connectionString = "User ID=postgres;Password=admin;server=10.3.2.188;Port=5432;Database=testorm;Pooling=true;";
+        //private const string connectionString = "User ID=postgres;Password=admin;server=10.3.2.188;Port=5432;Database=testorm;Pooling=true;";
         //private const string connectionString = "name=npgsql";
-        private const string DefaultDatabaseSchema = "public";
+        //private const string DefaultDatabaseSchema = "public";
+
+        // mysql
+        private const string connectionString = "Data Source=10.3.2.188;port=3306;Initial Catalog=testorm;user id=sa;password=#db877350;";
+        //private const string connectionString = "name=mysql";
+        private const string DefaultDatabaseSchema = "";//testorm
 
         protected override void Load(ContainerBuilder builder)
         {
