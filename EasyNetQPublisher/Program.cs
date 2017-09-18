f38565745eca93a24f1ed2b7908f3d76c2631686
GYC <GYC@BG001991>
2017-09-18 16:48:45 +0800

    增加EasyNetQ的Request/Respond、Publish/Subscribe等代码。

---
+++ EasyNetQPublisher/Program.cs
@@ -0,0 +1,43 @@
+﻿using EasyNetQ;
+using EasyNetQMessages;
+using System;
+using System.Threading.Tasks;
+
+namespace EasyNetQPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus(TextMessage.ConnectionString))
+            {
+                #region 测试Request/Respond
+
+                bus.RespondAsync<TextMessage, TextMessage>(
+                    request =>
+                        Task.Factory.StartNew(
+                            () => new TextMessage { Text = "Response from: " + request.Text }
+                        )
+                );
+
+                //bus.Respond<TextMessage, TextMessage>(request => new TextMessage { Text = "Response from: " + request.Text } );
+
+                #endregion
+
+                var input = "";
+                Console.WriteLine("Enter a message. 'Quit' to quit.");
+                while ((input = Console.ReadLine()) != "Quit")
+                {
+                    #region 测试Subscribe/Publish
+
+                    bus.Publish(new TextMessage
+                    {
+                        Text = input
+                    });
+
+                    #endregion
+                }
+            }
+        }
+    }
+}
---
+++ EasyNetQSubscriber/Program.cs
@@ -0,0 +1,56 @@
+﻿using EasyNetQ;
+using EasyNetQMessages;
+using System;
+
+namespace EasyNetQSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus(TextMessage.ConnectionString))
+            {
+                #region 测试Send/Receive
+
+                bus.Receive<TextMessage>("my.queue", message => Console.WriteLine("MyMessage: {0}", message.Text));
+                bus.Send("my.queue", new TextMessage { Text = "Hello Widgets!" });
+
+                #endregion
+
+                TestSubscribe_Publish(bus);
+
+                TestRequest_Respond( bus );
+
+                Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                Console.ReadLine();
+            }
+        }
+
+        static void TestRequest_Respond(IBus bus)
+        {
+            #region 测试Request/Respond
+
+            var request = new TextMessage { Text = "Hello Server" };
+            var response = bus.Request<TextMessage, TextMessage>(request);
+            Console.WriteLine($"Request: {request.Text}, Response: {response.Text}");
+
+            #endregion
+        }
+
+        static void TestSubscribe_Publish(IBus bus)
+        {
+            #region 测试Subscribe/Publish
+
+            bus.Subscribe<TextMessage>("test", m => HandleTextMessage(m));
+
+            #endregion
+        }
+
+        static void HandleTextMessage(TextMessage textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got message: {0}", textMessage.Text);
+            Console.ResetColor();
+        }
+    }
+}
