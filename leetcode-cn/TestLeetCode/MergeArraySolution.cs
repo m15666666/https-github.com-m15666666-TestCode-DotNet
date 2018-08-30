c1dfcd5e5fed70f55c9a2790538719c565b2bb8d
m15666666 <m15666666@126.com>
2018-08-30 08:57:25 +0800

    增加ergeArray、SorteArrayToBST.

---
+++ leetcode-cn/TestLeetCode/MergeArraySolution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+class MergeArraySolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        if (n == 0) return;
+        if (m == 0 && 0 < n)
+        {
+            nums2.CopyTo(nums1,0);
+            return;
+        }
+
+        int lastIndex = m + n - 1;
+        int mLastIndex = m - 1;
+        int nLastIndex = n - 1;
+
+        while( -1< lastIndex && -1 < nLastIndex)
+        {
+            var nValue = nums2[nLastIndex];
+
+            if (mLastIndex < 0)
+            {
+                nums1[lastIndex--] = nValue;
+                nLastIndex--;
+                continue;
+            }
+
+            var mValue = nums1[mLastIndex];
+            if (mValue <= nValue)
+            {
+                nums1[lastIndex--] = nValue;
+                nLastIndex--;
+            }
+            else
+            {
+                nums1[lastIndex--] = mValue;
+                mLastIndex--;
+            }
+        }
+    }
+}
---
+++ leetcode-cn/TestLeetCode/SortedArrayToBSTSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+class SortedArrayToBSTSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public TreeNode SortedArrayToBST(int[] nums)
+    {
+        if (nums == null || nums.Length == 0) return null;
+        return SortedArrayToBST(nums, 0, nums.Length);
+    }
+
+    private TreeNode SortedArrayToBST(int[] nums, int index, int length)
+    {
+        if ( length < 1) return null;
+
+        int lastIndex = index + length - 1;
+
+        if (length == 1) return new TreeNode(nums[index]);
+        if (length == 2) return new TreeNode(nums[index + 1]) { left = new TreeNode(nums[index]) };
+        if (length == 3) return new TreeNode(nums[index + 1]) { left = new TreeNode(nums[index]), right = new TreeNode(nums[index+2]) };
+
+        int midIndex = index + length / 2;
+        var ret = new TreeNode(nums[midIndex]);
+        ret.left = SortedArrayToBST(nums, index, midIndex - 1 - index + 1);
+        ret.right = SortedArrayToBST(nums, midIndex + 1, lastIndex - (midIndex + 1) + 1);
+        return ret;
+    }
+}
