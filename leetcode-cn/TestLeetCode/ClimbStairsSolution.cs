37bca3b112fc4fc6680f52e6fde2e214608097eb
m15666666 <m15666666@126.com>
2018-08-31 10:43:30 +0800

    增加ClimbStars。

---
+++ leetcode-cn/TestLeetCode/ClimbStairsSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+/// </summary>
+class ClimbStairsSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    
+    public int ClimbStairs(int n)
+    {
+        Dictionary<int, int> n2PathCount = new Dictionary<int, int>();
+        return ClimbStairs(n, n2PathCount);
+    }
+
+    private int ClimbStairs(int n, Dictionary<int, int> n2PathCount)
+    {
+        if (n <= 1) return 1;
+        if (n == 2) return 2;
+
+        if (n2PathCount.ContainsKey(n)) return n2PathCount[n];
+
+        var ret = ClimbStairs(n - 1, n2PathCount) + ClimbStairs(n - 2, n2PathCount);
+
+        if (!n2PathCount.ContainsKey(n)) n2PathCount[n] = ret;
+
+        return ret;
+    }
+}
---
+++ leetcode-cn/TestLeetCode/FirstBadVersionSolution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+class FirstBadVersionSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public int FirstBadVersion(int n)
+    {
+        if (n <= 1) return n;
+
+        long first = 1;
+        long last = n;
+
+        while (first < last)
+        {
+            long half = (first + last) / 2;
+            Console.WriteLine($"half:{half}");
+            if (IsBadVersion((int)half))
+            {
+                last = half;
+            }
+            else
+            {
+                first = half + 1;
+            }
+        }
+        return (int)last;
+    }
+
+    private bool IsBadVersion(int n)
+    {
+        return true;
+    }
+}
