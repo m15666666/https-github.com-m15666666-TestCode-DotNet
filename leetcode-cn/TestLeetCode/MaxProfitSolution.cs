880e5e12ad5da82f67f2067f0e2931b8b2e5f52f
m15666666 <m15666666@126.com>
2018-09-03 11:25:50 +0800

    增加最大子数组和。

---
+++ leetcode-cn/TestLeetCode/MaxProfitSolution.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+/// </summary>
+class MaxProfitSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+
+    public int MaxProfit(int[] prices)
+    {
+        if (prices == null || prices.Length < 1) return 0;
+
+        int max = 0;
+        int min = prices[0];
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (prices[i] < min)
+                min = prices[i];
+            else
+            {
+                if (max < prices[i] - min)
+                    max = prices[i] - min;
+            }
+        }
+        return max;
+    }
+
+    private int PartialMinIndex(int[] prices, int startIndex)
+    {
+        if (prices.Length <= startIndex) return -1;
+
+        var min = prices[startIndex];
+        var minIndex = startIndex;
+        for (int i = startIndex + 1; i < prices.Length; i++)
+        {
+            var v = prices[i];
+            if (min < v)
+            {
+                return minIndex;
+            }
+            if (v <= min)
+            {
+                minIndex = i;
+                min = v;
+            }
+        }
+        return minIndex;
+    }
+
+    private int PartialMaxIndex(int[] prices, int startIndex)
+    {
+        if (prices.Length <= startIndex) return -1;
+
+        var max = prices[startIndex];
+        var maxIndex = startIndex;
+        for (int i = startIndex + 1; i < prices.Length; i++)
+        {
+            var v = prices[i];
+            if ( v <= max )
+            {
+                return maxIndex;
+            }
+            if ( max < v )
+            {
+                maxIndex = i;
+                max = v;
+            }
+        }
+        return maxIndex;
+    }
+
+}
---
+++ leetcode-cn/TestLeetCode/MaxSubArraySolution.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+/// https://blog.csdn.net/qq_18124075/article/details/80515104
+/// </summary>
+class MaxSubArraySolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+
+    /**
+	* @param nums: A list of integers
+	* @return: A integer indicate the sum of max subarray
+	*/
+
+    /*
+		int maxSubArray(vector<int> &nums) {
+		// write your code here
+		// 分治思想
+		int begin = 0, end = nums.size() - 1;
+		return maxArray(nums, begin, end);
+	}
+	int maxArray(vector<int> &nums, int begin, int end){
+		if (begin == end)
+			return nums[begin];
+		int mid = (begin + end) >> 1;
+		int m1 = maxArray(nums, begin, mid);
+		int m2 = maxArray(nums, mid + 1, end);
+		// 从中向左地寻找最大和
+		int i, left = nums[mid], now = nums[mid];
+		for (i = mid-1; i >= begin; i--){
+			now+= nums[i];
+			left = max(now, left);
+		}
+		// 从中向右地寻找最大和
+		int right = nums[mid + 1];
+		now = nums[mid + 1];
+		for (i = mid+2; i <= end; i++){
+			now += nums[i];
+			right = max(right, now);
+		}
+		int m3 = left + right;
+		return max(m1, max(m2, m3));
+	}
+	*/
+    int MaxSubArray(int[] nums)
+    {
+        if (nums == null || nums.Length == 0) return 0;
+
+        // write your code here
+        // 动态规划
+        int result = nums[0];
+        int sum = nums[0];
+        int len = nums.Length;
+        for (int i = 1; i < len; i++)
+        {
+            if (sum > 0)
+                sum += nums[i];
+            else
+                sum = nums[i];
+            if (result < sum)
+                result = sum;
+        }
+        return result;
+    }
+}
