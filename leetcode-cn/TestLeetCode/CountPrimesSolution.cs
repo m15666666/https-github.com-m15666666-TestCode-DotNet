a1cf0d05e30ac305c35c91ff8fbd5919d2ab70b8
m15666666 <m15666666@126.com>
2018-09-10 09:16:05 +0800

    统计所有小于非负整数 n 的质数的数量。

---
+++ leetcode-cn/TestLeetCode/CountPrimesSolution.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// 统计所有小于非负整数 n 的质数的数量。
+/// https://blog.csdn.net/qq_27093465/article/details/59576577
+/// https://blog.csdn.net/jianchilu/article/details/79439868
+/// </summary>
+class CountPrimesSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+
+    public int CountPrimes(int n)
+    {
+        var primesHelper = new PrimesHelper(n-1);
+        return primesHelper.CountPrimes();
+    }
+
+    public class PrimesHelper
+    {
+        private int N { get; set; }
+
+        private List<int> Primes { get; set; }  = new List<int>();
+        private int _currentBiggestPrime = 7;
+        private int _primeCount = 0;
+
+        public PrimesHelper(int n)
+        {
+            N = n;
+
+            Primes.AddRange(new []{2,3,5,7});
+
+            Init();
+        }
+
+        private void Init()
+        {
+            int n = N;
+            if (n < 3) return;
+
+            int squareRoot = 2;
+            int square = squareRoot * squareRoot;
+            _primeCount = 1;
+            int currentN = 3;
+
+            while (true)
+            {
+                // 计算currentN是否是质数
+                if (currentN <= square)
+                {
+                    // do ...
+                    bool isPrime = true;
+                    foreach (var prime in Primes)
+                    {
+                        if (squareRoot < prime) break;
+                        if (currentN % prime == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+
+                    if (isPrime)
+                    {
+                        _primeCount++;
+                        if (_currentBiggestPrime < currentN)
+                        {
+                            _currentBiggestPrime = currentN;
+                            Primes.Add(currentN);
+                        }
+                    }
+
+                    currentN++;
+                    if (n < currentN) return;
+                }
+                else
+                {
+                    squareRoot++;
+                    square = squareRoot * squareRoot;
+                    continue;
+                }
+            }
+        }
+
+        public int CountPrimes()
+        {
+            int n = N;
+            if (n < 2) return 0;
+            if (n == 2) return 1;
+
+            return _primeCount;
+        }
+    }
+}
---
+++ leetcode-cn/TestLeetCode/FizzBuzzSolution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// 写一个程序，输出从 1 到 n 数字的字符串表示。
+/// </summary>
+class FizzBuzzSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+
+    public IList<string> FizzBuzz(int n)
+    {
+        List<string> ret = new List<string>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                ret.Add("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                ret.Add("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                ret.Add("Buzz");
+            }
+            else
+            {
+                ret.Add($"{i}");
+            }
+        }
+
+        return ret;
+    }
+}
