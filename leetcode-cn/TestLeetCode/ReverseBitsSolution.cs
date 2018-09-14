a08d1c979acf941b83567bd2cabcef9859eca254
m15666666 <m15666666@126.com>
2018-09-15 07:00:57 +0800

    颠倒给定的 32 位无符号整数的二进制位。
    示例:
    
    输入: 43261596
    输出: 964176192
    解释: 43261596 的二进制表示形式为 00000010100101000001111010011100 ，
         返回 964176192，其二进制表示形式为 00111001011110000010100101000000 。

---
+++ leetcode-cn/TestLeetCode/ReverseBitsSolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// 颠倒给定的 32 位无符号整数的二进制位。
+/// Input: 43261596
+/// Output: 964176192 
+/// Explanation: 43261596 represented in binary as 00000010100101000001111010011100, 
+/// return 964176192 represented in binary as 00111001011110000010100101000000.
+/// 
+/// https://blog.csdn.net/hy971216/article/details/80524704
+/// </summary>
+class ReverseBitsSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public uint reverseBits(uint n)
+    {
+        uint reverse_num = 0;
+        const int bitCount = 32;
+        for ( int i = 0; i < bitCount; i++)
+        {
+           uint temp = (n & (uint)(1 << i));
+            if (temp != 0)
+                reverse_num |= (uint)(1 << ((bitCount - 1) - i));
+        }
+
+        return reverse_num;
+    }
+}
