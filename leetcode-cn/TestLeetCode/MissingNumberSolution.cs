3695fc2e3ddf9bd1c754b552916da0ac1d0603e2
m15666666 <m15666666@126.com>
2018-09-18 09:25:19 +0800

    缺失数字 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。

---
+++ leetcode-cn/TestLeetCode/MissingNumberSolution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// 缺失数字
+/// 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+/// </summary>
+class MissingNumberSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public int MissingNumber(int[] nums)
+    {
+        Array.Sort(nums);
+        int index = 0;
+
+        foreach( var v in nums)
+        {
+            if (index != v) return index;
+            index++;
+        }
+
+        return index;
+    }
+}
