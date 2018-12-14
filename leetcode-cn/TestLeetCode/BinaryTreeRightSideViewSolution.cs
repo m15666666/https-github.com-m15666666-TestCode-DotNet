a0561977faaba1abc55f93480caac4d3750cb023
m15666666 <m15666666@126.com>
2018-12-14 08:27:45 +0800

    199. 二叉树的右视图

---
+++ leetcode-cn/TestLeetCode/BinaryTreeRightSideViewSolution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// https://leetcode-cn.com/problems/binary-tree-right-side-view/
+/// 199. 二叉树的右视图
+/// 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+/// </summary>
+class BinaryTreeRightSideViewSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+
+    public IList<int> RightSideView(TreeNode root)
+    {
+        List<int> ret = new List<int>();
+        if( root != null ) RightSideView(root, 1, ret);
+        return ret;
+    }
+
+    private void RightSideView( TreeNode root, int level, List<int> ret )
+    {
+        if( ret.Count < level ) ret.Add(root.val);
+        if( root.right != null ) RightSideView( root.right, level + 1, ret );
+        if (root.left != null) RightSideView(root.left, level + 1, ret);
+    }
+}
+
+/*
+ // 别人的算法
+
+public class Solution {
+    public IList<int> list = new List<int>();
+    
+    public IList<int> RightSideView(TreeNode root, int layer = 0) {
+        if(root == null)
+            return list;
+        
+        while(list.Count <= layer)
+            list.Add(0);
+        
+        list[layer] = root.val;
+        
+        RightSideView(root.left, layer + 1);
+        RightSideView(root.right, layer + 1);
+        
+        return list;
+    }
+}
+
+public class Solution {
+    List<int> ret;
+    void dfs(TreeNode tn, int dep) {
+        if(dep >= ret.Count) ret.Add(tn.val);
+        else ret[dep] = tn.val;
+        if(tn.left != null) dfs(tn.left, dep + 1);
+        if(tn.right != null) dfs(tn.right, dep + 1);
+    }
+    public IList<int> RightSideView(TreeNode root) {
+        ret = new List<int>();
+        if(root != null) dfs(root, 0);
+        return ret;
+    }
+}
+*/
---
+++ leetcode-cn/TestLeetCode/RepeatedDNASequencesSolution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// https://leetcode-cn.com/problems/repeated-dna-sequences/
+/// 187. 重复的DNA序列
+/// 所有 DNA 由一系列缩写为 A，C，G 和 T 的核苷酸组成，例如：“ACGAATTCCG”。在研究 DNA 时，识别 DNA 中的重复序列有时会对研究非常有帮助。
+/// 编写一个函数来查找 DNA 分子中所有出现超多一次的10个字母长的序列（子串）。
+/// 示例:
+/// 输入: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+/// 输出: ["AAAAACCCCC", "CCCCCAAAAA"]
+/// https://blog.csdn.net/w8253497062015/article/details/80672207
+/// </summary>
+class RepeatedDNASequencesSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+
+    public IList<string> FindRepeatedDnaSequences(string s)
+    {
+        List<string> ret = new List<string>();
+        if (s == null || s.Length < 10) return ret;
+
+        Dictionary<string, int> dnaCount = new Dictionary<string, int>();
+        int lastIndex = s.Length - 10;
+        for( int i = 0; i <= lastIndex; i++)
+        {
+            var str = s.Substring(i, 10);
+            if (dnaCount.ContainsKey(str)) dnaCount[str] = 2;
+            else dnaCount[str] = 1;
+        }
+        foreach( var pair in dnaCount)
+        {
+            if (1 < pair.Value) ret.Add(pair.Key);
+        }
+        return ret;
+    }
+}
