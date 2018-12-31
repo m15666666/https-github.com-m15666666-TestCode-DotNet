4fd34a47aa8efc6b30d29aafe30970156e186f4e
m15666666 <m15666666@126.com>
2018-12-31 22:24:22 +0800

    222. 完全二叉树的节点个数

---
+++ leetcode-cn/TestLeetCode/CountCompleteTreeNodesSolution.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// https://leetcode-cn.com/problems/count-complete-tree-nodes/
+/// 222. 完全二叉树的节点个数
+/// 给出一个完全二叉树，求出该树的节点个数。
+/// 说明：
+/// 完全二叉树的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，
+/// 并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+/// 示例:
+/// 输入: 
+/// 1
+/// / \
+/// 2   3
+/// / \  /
+/// 4  5 6
+/// 输出: 6
+/// http://dongcoder.com/detail-1113988.html
+/// </summary>
+class CountCompleteTreeNodesSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+
+    // 获取左子树的高度(其实是最左侧分支）
+    public int GetLeftHeight(TreeNode root)
+    {
+        int count = 0;
+        while (root != null)
+        {
+            count++;
+            root = root.left;
+        }
+        return count;
+    }
+
+    // 获取右子树的高度（其实是最右侧分支的高度）
+    public int GetRightHeight(TreeNode root)
+    {
+        int count = 0;
+        while (root != null)
+        {
+            count++;
+            root = root.right;
+        }
+        return count;
+    }
+
+    public int CountNodes(TreeNode root)
+    {
+        if (root == null) return 0;
+
+        int leftHeight = GetLeftHeight(root);
+        int rightHeight = GetRightHeight(root);
+
+        if (leftHeight == rightHeight)
+        {
+            // 表示是满二叉树，二叉树的节点数直接由公式2^n-1得到
+            // leftHeight即为层数， 1 << leftHeight使用位运算计算2^leftHeight，效率更高
+            // 注意(1 << leftHeight) - 1 的括号必须有！！
+            return (1 << leftHeight) - 1;
+        }
+
+        // 若该二叉树不是满二叉树，递归的调用该方法，计算左子树和右子树的节点数
+        return CountNodes(root.left) + CountNodes(root.right) + 1;
+    }
+}
+/*
+//别人的算法
+public class Solution {
+    public int CountNodes(TreeNode root) {
+        if(root==null)return 0;//判空返回
+        //获取左右子树高度
+        int leftHeight=GetHeight(root.left);
+        int rigthHeight=GetHeight(root.right);
+        //相等说明左子树一定是满的，返回左子树节点数和右子树递归，否则先计算左子树递归
+        if(leftHeight==rigthHeight)return (1<<leftHeight)+CountNodes(root.right);
+        else return (1<<rigthHeight)+CountNodes(root.left);
+    }
+    public int GetHeight(TreeNode node){
+        //左右子树获取高度都是取其左子树遍历
+        //左右子树若相等，最后的叶节点一定在右子树，若不相等则右子树高度小于左子树
+        int height = 0;
+        while(node != null) {
+            height++;
+            node = node.left;
+        }
+        return height;
+    }
+}
+*/
