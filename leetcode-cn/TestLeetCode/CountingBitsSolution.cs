f1051e6aa063786eccb07b7506c66eec2195aff1
m15666666 <m15666666@126.com>
2019-03-25 10:51:54 +0800

    341. 扁平化嵌套列表迭代器

---
+++ leetcode-cn/TestLeetCode/CountingBitsSolution.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
+
+示例 1:
+
+输入: 2
+输出: [0,1,1]
+示例 2:
+
+输入: 5
+输出: [0,1,1,2,1,2]
+进阶:
+
+给出时间复杂度为O(n*sizeof(integer))的解答非常容易。但你可以在线性时间O(n)内用一趟扫描做到吗？
+要求算法的空间复杂度为O(n)。
+你能进一步完善解法吗？要求在C++或任何其他语言中不使用任何内置函数（如 C++ 中的 __builtin_popcount）来执行此操作。 
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/counting-bits/
+/// 338. 比特位计数
+/// </summary>
+class CountingBitsSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public int[] CountBits(int num)
+    {
+        if (num < 0) return new int[0];
+
+        var ret = new int[num + 1];
+        ret[0] = 0;
+        if (num == 0) return ret;
+
+        ret[1] = 1;
+        if (num == 1) return ret;
+
+        var slowIndex = 1;
+        for(var i = 2; i <= num; i++ )
+            if( (slowIndex + slowIndex) == i) ret[i] = ret[slowIndex++];
+            else ret[i] = ret[i - 1] + 1;
+
+        return ret;
+    }
+}
+/*
+public class Solution {
+    public int[] CountBits(int num)
+    {
+        int[] array = new int[num + 1];
+
+        array[0] = 0;
+        if (num > 0)
+            array[1] = 1;
+
+        for (int i = 2; i <= num; i++)
+        {
+            int tep = i;
+            array[i] = array[tep % 2];
+            tep = tep >> 1;
+            array[i] += array[tep];
+        }
+
+        return array;
+    }
+}
+public class Solution {
+    public int[] CountBits(int num)
+    {
+        int[] array = new int[num + 1];
+        for (int i = 0; i <= num; i++)
+        {
+            array[i] = PopCount(i);
+        }
+
+        return array;
+    }
+
+    private static int PopCount(int n)
+    {
+        int count = 0;
+        while (n > 0)
+        {
+            if (n % 2 == 1)
+            {
+                count++;
+            }
+
+            n /= 2;
+        }
+
+        return count;
+    }
+}
+public class Solution {
+    public int[] CountBits(int num) {
+        int[] result=new int[num+1];
+        result[0]=0;
+        for(int i=1;i<result.Length;i++){
+            if(i%2==0) result[i]=result[i/2];
+            else result[i]=result[i-1]+1;
+        }
+        return result;
+    }
+}
+*/
---
+++ leetcode-cn/TestLeetCode/FlattenNestedListIteratorSolution.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+给定一个嵌套的整型列表。设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
+
+列表中的项或者为一个整数，或者是另一个列表。
+
+示例 1:
+
+输入: [[1,1],2,[1,1]]
+输出: [1,1,2,1,1]
+解释: 通过重复调用 next 直到 hasNext 返回false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+示例 2:
+
+输入: [1,[4,[6]]]
+输出: [1,4,6]
+解释: 通过重复调用 next 直到 hasNext 返回false，next 返回的元素的顺序应该是: [1,4,6]。 
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/flatten-nested-list-iterator/
+/// 341. 扁平化嵌套列表迭代器
+/// https://blog.csdn.net/zrh_CSDN/article/details/83897581
+/// </summary>
+class NestedIterator
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    private Stack<NestedInteger> _stack = new Stack<NestedInteger>();
+    public NestedIterator(IList<NestedInteger> nestedList)
+    {
+        if (nestedList != null && 0 < nestedList.Count )
+            for (int i = nestedList.Count - 1; -1 < i; --i ) _stack.Push(nestedList[i]);
+    }
+
+    public bool HasNext()
+    {
+        while ( 0 < _stack.Count )
+        {
+            NestedInteger t = _stack.Peek();
+            if (t.IsInteger()) return true;
+
+            var list = _stack.Pop().GetList();
+            for ( int i = list.Count - 1; -1 < i; --i) _stack.Push(list[i] );
+        }
+        return false;
+    }
+
+    public int Next()
+    {
+        NestedInteger t = _stack.Pop();
+        return t.GetInteger();
+    }
+}
+/*
+public class NestedIterator {
+
+    private List<Int32> items;
+    private Int32 index;
+    public NestedIterator(IList<NestedInteger> nestedList)
+    {
+        items = new List<Int32>();
+        index = 0;
+        Read(nestedList);
+    }
+
+    public void Read(IList<NestedInteger> list)
+    {
+        for(Int32 i = 0; i < list.Count; ++i)
+        {
+            if(list[i].IsInteger())
+            {
+                items.Add(list[i].GetInteger());
+            }
+            else
+            {
+                Read(list[i].GetList());
+            }
+        }
+    }
+
+    public bool HasNext()
+    {
+        if(index >= items.Count)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public int Next()
+    {
+        return items[index++];
+    }
+}
+public class NestedIterator {
+   private Stack<Cursor> stack = new Stack<Cursor>();
+    public NestedIterator(IList<NestedInteger> nestedList) {
+        if (nestedList != null) stack.Push(new Cursor(nestedList));
+    }
+
+    public bool HasNext() {
+        while (stack.Count!=0)
+            {
+                Cursor cursor = stack.Peek();
+                if (cursor.i < cursor.list.Count)
+                {
+                    NestedInteger nested = cursor.list[cursor.i];
+                    if (nested.IsInteger()) return true;
+                    cursor.i++;
+                    stack.Push(new Cursor(nested.GetList()));
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+            return false;
+    }
+
+    public int Next() {
+         while (stack.Count!=0)
+            {
+                Cursor cursor = stack.Peek();
+                if (cursor.i < cursor.list.Count)
+                {
+                    NestedInteger nested = cursor.list[cursor.i++];
+                    if (nested.IsInteger()) return nested.GetInteger();
+                    stack.Push(new Cursor(nested.GetList()));
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+            return -1;
+    }
+     public  class Cursor
+    {
+       public   IList<NestedInteger> list;
+       public  int i;
+       public   Cursor(IList<NestedInteger> list)
+        {
+            this.list = list;
+        }
+    }
+}
+public class NestedIterator {
+    private Queue<int> q;
+
+    public NestedIterator(IList<NestedInteger> nestedList) {
+        q = new Queue<int>();
+        
+        Init(nestedList);
+    }
+    
+    private void Init(IList<NestedInteger> nestedList)
+    {
+        foreach(NestedInteger item in nestedList)
+        {
+            if(item.IsInteger())
+            {
+                q.Enqueue(item.GetInteger());
+            }
+            else
+            {
+                Init(item.GetList());
+            }
+        }
+    }
+
+    public bool HasNext() {
+        return q.Count() != 0;
+    }
+
+    public int Next() {
+        return q.Dequeue();
+    }
+}
+public class NestedIterator {
+    private Stack<Cursor> stack = new Stack<Cursor>();
+    public NestedIterator(IList<NestedInteger> nestedList) {
+        stack.Push(new Cursor(nestedList));
+    }
+
+    public bool HasNext() {
+          while (stack.Count!=0)
+            {
+                Cursor state = stack.Peek();
+                if (state.i < state.list.Count)
+                {
+                    if (state.list[state.i].IsInteger()) return true;
+                    stack.Push(new Cursor(state.list[state.i++].GetList()));
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+            return false;
+    }
+
+    public int Next() {
+          HasNext();
+            Cursor state = stack.Peek();
+            return state.list [state.i++].GetInteger();
+    }
+     public  class Cursor
+    {
+       public   IList<NestedInteger> list;
+       public  int i;
+       public   Cursor(IList<NestedInteger> list)
+        {
+            this.list = list;
+        }
+    }
+}
+
+*/
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool IsInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     int GetInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     IList<NestedInteger> GetList();
+ * }
+ */
+interface NestedInteger
+{
+ 
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    bool IsInteger();
+ 
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    int GetInteger();
+ 
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return null if this NestedInteger holds a single integer
+    IList<NestedInteger> GetList();
+}
