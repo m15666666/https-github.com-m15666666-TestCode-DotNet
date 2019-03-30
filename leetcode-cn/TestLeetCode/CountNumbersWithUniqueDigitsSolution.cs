bdd7fa45916c7a1b0be5a3e71c09020e6d163219
m15666666 <m15666666@126.com>
2019-03-30 21:06:04 +0800

    357. 计算各个位数不同的数字个数 365. 水壶问题

---
+++ leetcode-cn/TestLeetCode/CountNumbersWithUniqueDigitsSolution.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+给定一个非负整数 n，计算各位数字都不同的数字 x 的个数，其中 0 ≤ x < 10n 。
+
+示例:
+
+输入: 2
+输出: 91 
+解释: 答案应为除去 11,22,33,44,55,66,77,88,99 外，在 [0,100) 区间内的所有数字。 
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/count-numbers-with-unique-digits/
+/// 357. 计算各个位数不同的数字个数
+/// </summary>
+class CountNumbersWithUniqueDigitsSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public int CountNumbersWithUniqueDigits(int n)
+    {
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+        if (n == 1) return 10;
+
+        int ret = 9;
+        int digitCount = n - 1;
+        int choiceCount = 9;
+        while (0 < digitCount--) ret *= choiceCount--;
+
+        return ret + CountNumbersWithUniqueDigits(n - 1);
+    }
+}
+/*
+public class Solution {
+    public int CountNumbersWithUniqueDigits(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        if (n == 1)
+        {
+            return 10;
+        }
+        int[] dp = new int[n +1];
+        dp[0] = 1;
+        dp[1] = 10;
+        for (int i = 2; i <= Math.Min(n,10); i++)
+        {
+            dp[i] = dp[i - 1] + (dp[i - 1] - dp[i - 2]) * (11 - i);
+        }
+        if (n > 10)
+        {
+            return dp[10];
+        }
+        return dp[n];
+    }
+} 
+public class Solution {
+    public int CountNumbersWithUniqueDigits(int n)
+        {
+            if (n > 9 || n < 1)
+                return 1;
+            int[] arr = new int[n];
+            arr[0] = 10;
+            for (int i = 1; i<n ;i++)
+            {
+                int num = 9 ,sum = 1;
+                for (int j = i; j > 0; j--)
+                {
+                    sum = num * sum;
+                    num--;
+                }
+                sum *= 9;
+                arr[i] = sum + arr[i - 1];
+            }
+            return arr[n-1];
+        }
+}
+public class Solution {
+    public int CountNumbersWithUniqueDigits(int n) {
+        if(n==0)
+            return 1;
+          if (n > 10)
+                n = 10;
+            var res = 10;
+            for (var i = 2; i <= n; i++) {
+                var k = 9;
+                for (var j = 0; j < i-1; j++) {
+                    k *= (9 - j);
+                }
+                res += k;
+            }
+            return res;
+    }
+}
+public class Solution {
+    public int CountNumbersWithUniqueDigits(int n) {
+        if(n == 0){
+            return 1;
+        }
+        int r = 10;
+        int s = 9;
+        for(int i = 2; i <= n; i ++) {
+            s *= (10 - i + 1);
+            r += s;
+        }
+        return r;
+    }
+}
+public class Solution {
+    public int CountNumbersWithUniqueDigits(int n)
+    {
+        if (n < 0)
+            return 0;
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return 10;
+        if (n == 2)
+            return 91;
+        if (n == 3)
+            return 739;
+        if (n == 4)
+            return 5275;
+        if (n == 5)
+            return 32491;
+        if (n == 6)
+            return 168571;
+        if (n == 7)
+            return 712891;
+        if (n == 8)
+            return 2345851;
+        if (n == 9)
+            return 4791702;
+        else
+            return 7419072;
+    }
+}
+public class Solution {
+    public int CountNumbersWithUniqueDigits (int n) {
+        switch(n){
+            case 0: return 1;
+            case 1: return 10;
+            case 2: return 91;
+        }
+        if(n>=10){
+            return CountNumbersWithUniqueDigits(9);
+        }
+          
+        return CountNumbersWithUniqueDigits(n-1)+9*Permutation(9,n-1);
+    }
+    private int Permutation(int n,int i){
+        int s=1;
+        for (int k = 0; k < i; k++)
+        {
+            s*=n-k;
+        }
+        return s;
+    }
+}
+public class Solution {
+    public int CountNumbersWithUniqueDigits(int n) {
+        if (n > 10) n = 10;
+        var arr = new int[11] { 1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851, 5611771, 8877691 };
+        return arr[n];
+    }
+}
+*/
---
+++ leetcode-cn/TestLeetCode/WaterAndJugProblemSolution.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+有两个容量分别为 x升 和 y升 的水壶以及无限多的水。请判断能否通过使用这两个水壶，从而可以得到恰好 z升 的水？
+
+如果可以，最后请用以上水壶中的一或两个来盛放取得的 z升 水。
+
+你允许：
+
+装满任意一个水壶
+清空任意一个水壶
+从一个水壶向另外一个水壶倒水，直到装满或者倒空
+示例 1: (From the famous "Die Hard" example)
+
+输入: x = 3, y = 5, z = 4
+输出: True
+示例 2:
+
+输入: x = 2, y = 6, z = 5
+输出: False 
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/water-and-jug-problem/
+/// 365. 水壶问题
+/// https://www.cnblogs.com/grandyang/p/5628836.html
+/// </summary>
+class WaterAndJugProblemSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public bool CanMeasureWater(int x, int y, int z)
+    {
+        return z == 0 || (x + y >= z && z % Gcd(x, y) == 0);
+    }
+
+    int Gcd(int x, int y)
+    {
+        return y == 0 ? x : Gcd( y, x % y );
+    }
+}
+/*
+public class Solution
+{
+    public bool CanMeasureWater (int x, int y, int z)
+    {
+        if(z==0) return true;
+        if(z>(x+y)) return false;
+        while(x!=y){
+            if(x<y){
+                int t=x;
+                x=y;
+                y=t;
+            }
+            if(y==0) break;
+            x=x-y;
+        }
+        if(x==0) return false;
+        return z%x==0;
+    }
+}
+public class Solution {
+    public bool CanMeasureWater(int x, int y, int z)
+    {
+        if(z==0)
+            return true;
+        if(x==0)
+            return z==y;
+        if(y==0)
+            return z==x;
+        int m = Math.Max(x, y);
+        int n = Math.Min(x, y);
+        if(z>m+n)
+            return false;
+        while(m%n!=0){
+            int rem=m%n;
+            m=n;
+            n=rem;
+        }
+        return z%n==0;
+    }
+}
+public class Solution {
+    public bool CanMeasureWater(int x, int y, int z) {
+
+        if(((x==0&&y!=z)||(y==0&&x!=z))&&z!=0)
+            return false;
+        if(z>x+y)
+            return false;
+        if((x>z&&y>z&&Math.Abs(x-y)>z&&z>0)&&!(x%2==1&&y%2==1&&z%2==1))
+            return false;
+
+        if((x==2&&y==6&&z==5)||(x==6&&y==9&&z==1))
+            return false;
+        return true;
+    }
+}
+
+*/
