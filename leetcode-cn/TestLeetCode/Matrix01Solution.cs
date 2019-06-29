4d2ccbc56523dfa2bb5c6bb19f00fe45a9fa073a
m15666666 <m15666666@126.com>
2019-06-29 20:42:28 +0800

    542. 01 矩阵

--- leetcode-cn/TestLeetCode/Matrix01Solution.cs
+++ leetcode-cn/TestLeetCode/Matrix01Solution.cs
@@ -40,6 +40,7 @@ using System.Text;
 /// <summary>
 /// https://leetcode-cn.com/problems/01-matrix/
 /// 542. 01 矩阵
+/// https://www.jianshu.com/p/aecba8d8aca0
 /// </summary>
 class Matrix01Solution
 {
@@ -54,6 +55,91 @@ class Matrix01Solution
 
     public int[][] UpdateMatrix(int[][] matrix)
     {
+        int row = matrix.Length;
+        int col = matrix[0].Length;
+        Queue<int> q = new Queue<int>(128);
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    EnqueuePair(q,i,j);
+                }
+                else
+                {
+                    matrix[i][j] = int.MaxValue;
+                }
+            }
+        }
 
+        while ( 0 < q.Count )
+        {
+            int x = q.Dequeue();
+            int y = q.Dequeue();
+            int cur = matrix[x][y] + 1;
+            if (x > 0 && matrix[x - 1][y] > cur)
+            {
+                matrix[x - 1][y] = cur;
+                EnqueuePair(q, x - 1, y);
+            }
+            if (x < row - 1 && matrix[x + 1][y] > cur)
+            {
+                matrix[x + 1][y] = cur;
+                EnqueuePair(q, x + 1, y);
+            }
+            if (y > 0 && matrix[x][y - 1] > cur)
+            {
+                matrix[x][y - 1] = cur;
+                EnqueuePair(q, x, y - 1);
+            }
+            if (y < col - 1 && matrix[x][y + 1] > cur)
+            {
+                matrix[x][y + 1] = cur;
+                EnqueuePair(q, x, y + 1);
+            }
+        }
+        return matrix;
+
+        void EnqueuePair(Queue<int> queue, int x, int y)
+        {
+            queue.Enqueue(x); queue.Enqueue(y);
+        }
     }
 }
+/*
+public class Solution {
+    public int[][] UpdateMatrix(int[][] matrix) {
+        int m = matrix.Count();
+        int n = matrix[0].Count();
+        
+        var queue = new Queue<int[]>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.Enqueue(new int[] {i, j});
+                }
+                else {
+                    matrix[i][j] = int.MaxValue;
+                }
+            }
+        }
+        
+        var dirs = new int[][]{new int[]{-1, 0}, new int[]{1, 0}, new int[]{0, -1}, new int[]{0, 1}};
+        
+        while (queue.Count() != 0) {
+            int[] cell = queue.Dequeue();
+            foreach (int[] d in dirs) {
+                int r = cell[0] + d[0];
+                int c = cell[1] + d[1];
+                if (r < 0 || r >= m || c < 0 || c >= n || 
+                    matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
+                queue.Enqueue(new int[] {r, c});
+                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
+            }
+        }
+        
+        return matrix;
+    }
+}
+*/
