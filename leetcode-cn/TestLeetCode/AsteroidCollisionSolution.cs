9327c3e6a8d75763790c888c45b6c9df079d1e51
m15666666 <m15666666@126.com>
2019-08-19 09:13:29 +0800

    735. 行星碰撞

--- leetcode-cn/TestLeetCode/AsteroidCollisionSolution.cs
+++ leetcode-cn/TestLeetCode/AsteroidCollisionSolution.cs
@@ -48,7 +48,7 @@ asteroids = [-2, -1, 1, 2]
 /// <summary>
 /// https://leetcode-cn.com/problems/asteroid-collision/
 /// 735. 行星碰撞
-/// 
+/// https://blog.csdn.net/qiang_____0712/article/details/84900857
 /// </summary>
 class AsteroidCollisionSolution
 {
@@ -63,6 +63,123 @@ class AsteroidCollisionSolution
 
     public int[] AsteroidCollision(int[] asteroids)
     {
+        if (asteroids == null || asteroids.Length == 0) return new int[0];
 
+        Stack<int> stack = new Stack<int>();
+        foreach( var v in asteroids)
+        {
+            if (stack.Count == 0) //如果栈为空，就入栈
+            {
+                stack.Push(v);
+                continue;
+            }
+
+            var top1 = stack.Peek();
+            if ((top1 < 0 && v > 0) || (top1 * v > 0)) //如果当前行星和栈顶行星相背或同向运行，就入栈
+            {
+                stack.Push(v);
+                continue;
+            }
+
+            var absCurrent = Math.Abs(v);
+            var absTop1 = Math.Abs(top1);
+            if (absTop1 == absCurrent) //如果当前行星和栈顶行星相向而行且大小相同，同时爆炸
+            {
+                stack.Pop();
+                continue;
+            }
+
+            if (absCurrent < absTop1) continue;
+
+            stack.Pop(); // 如果当前行星和栈顶行星相向而行且栈顶小于当前,则弹出栈顶
+
+            bool flag = true; //flag标识当前行星是否会爆炸
+            while ( 0 < stack.Count )
+            {
+                var top2 = stack.Peek();
+                if (top2 <= 0) break;
+
+                var absTop2 = Math.Abs(top2);
+                if (absCurrent < absTop2) break;
+
+                if (absCurrent == absTop2)
+                {
+                    flag = false;
+                    stack.Pop();
+                    break;
+                }
+                stack.Pop(); // 如果当前行星和栈顶行星相向而行且栈顶小于当前,则弹出栈顶
+            }
+
+            if (flag && (stack.Count == 0 || (stack.Peek() * v > 0))) stack.Push(v);
+        }
+
+        int[] ret = new int[stack.Count];
+        for (int i = ret.Length - 1; -1 < i; i--)
+        {
+            ret[i] = stack.Pop();
+        }
+        return ret;
     }
 }
+/*
+public class Solution {
+    public int[] AsteroidCollision(int[] asteroids) {
+        Stack<int> stack=new Stack<int>();
+        
+        for(int i=0;i<asteroids.Length;i++)
+        {
+            if(stack.Count!=0)
+            {
+                if(asteroids[i]<0)
+                {
+                    if(stack.Peek()>0)
+                    {
+                        if(Math.Abs(asteroids[i])<stack.Peek())
+                        {
+                            continue;
+                        }
+                        
+                        if(Math.Abs(asteroids[i])==stack.Peek())
+                        {
+                            stack.Pop();
+                            continue;
+                        }
+                        
+                        if(Math.Abs(asteroids[i])>stack.Peek())
+                        {
+                            stack.Pop();
+                            i--;
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        stack.Push(asteroids[i]);
+                    }
+                }
+                else
+                {
+                    stack.Push(asteroids[i]);
+                }
+            }
+            else
+            {
+                stack.Push(asteroids[i]);
+            }
+        }
+        
+        if(stack.Count==0)
+            return new int[]{};            
+        
+        int[] arr=new int[stack.Count];
+        for(int i=arr.Length-1;i>=0;i--)
+        {
+            arr[i]=stack.Pop();
+        }
+        
+        return arr;
+    }
+}
+
+*/
