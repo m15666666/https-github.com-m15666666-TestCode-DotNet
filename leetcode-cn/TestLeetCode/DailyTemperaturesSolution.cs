9292f103f5a97f29f26ca559856df8c4f39aebfb
m15666666 <m15666666@126.com>
2019-08-21 09:16:41 +0800

    739. 每日温度

--- leetcode-cn/TestLeetCode/DailyTemperaturesSolution.cs
+++ leetcode-cn/TestLeetCode/DailyTemperaturesSolution.cs
@@ -29,6 +29,67 @@ class DailyTemperaturesSolution
 
     public int[] DailyTemperatures(int[] T)
     {
+        int[] ret = new int[T.Length];
+        var stack = new Stack<int>();
+        for (int i = T.Length - 1; -1 < i; i--)
+        {
+            var v = T[i];
+            while (0 < stack.Count && T[stack.Peek()] <= v )
+                stack.Pop();
 
+            if (stack.Count == 0) ret[i] = 0;
+            else ret[i] = stack.Peek() - i;
+
+            stack.Push(i);
+        }
+        return ret;
     }
 }
+/*
+public class Solution {
+    public int[] DailyTemperatures(int[] T) {
+      Stack<int> stack = new Stack<int>();
+
+       int size = T.Length;
+       int[] result = new int[size];
+
+       for (int i = 0; i < size; i++)
+       {
+           while (stack.Count!=0 && T[stack.Peek()] < T[i])
+           {
+               int t = stack.Peek();
+               stack.Pop();
+               result[t] = i - t;
+           }
+           stack.Push(i);
+       }
+
+       return result;
+    }
+} 
+public class Solution {
+    public int[] DailyTemperatures(int[] T) {
+        int[] result = new int[T.Length];
+        Stack<int> tempIDStack = new Stack<int>();
+        for (int i = 0; i < T.Length; i++)
+        {
+            if (tempIDStack.Count > 0)
+            {
+                while (T[i] > T[tempIDStack.Peek()])
+                {
+                    result[tempIDStack.Peek()] = i - tempIDStack.Pop();
+                    if (0 == tempIDStack.Count)
+                        break;
+                }
+                tempIDStack.Push(i);
+            }
+            else
+            {
+                tempIDStack.Push(i);
+            }
+        }
+
+        return result;
+    }
+}
+*/
