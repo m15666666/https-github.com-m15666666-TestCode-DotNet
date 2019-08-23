b0ca22c729b34288b72b0aa16e35927e80dcd413
m15666666 <m15666666@126.com>
2019-08-23 08:58:39 +0800

    743. 网络延迟时间

--- leetcode-cn/TestLeetCode/NetworkDelayTimeSolution.cs
+++ leetcode-cn/TestLeetCode/NetworkDelayTimeSolution.cs
@@ -21,7 +21,7 @@ times 的长度在 [1, 6000] 之间。
 /// <summary>
 /// https://leetcode-cn.com/problems/network-delay-time/
 /// 743. 网络延迟时间
-/// 
+/// https://blog.csdn.net/xuxuxuqian1/article/details/80709191
 /// </summary>
 class NetworkDelayTimeSolution
 {
@@ -36,6 +36,118 @@ class NetworkDelayTimeSolution
 
     public int NetworkDelayTime(int[][] times, int N, int K)
     {
+        int[] dist = new int[N +1];
+        Array.Fill(dist, int.MaxValue);
+        dist[0] = 0;
+        dist[K] = 0;
+        for (; ; )
+        {
+            bool changed = false;
+            foreach (var e in times)
+            {
+                int u = e[0], v = e[1], w = e[2];
+                if (dist[u] != int.MaxValue && dist[v] > dist[u] + w)
+                {
+                    dist[v] = dist[u] + w;
+                    changed = true;
+                }
+            }
+            if (!changed) break;
+        }
 
+        int ret = dist.Max();
+        return ret == int.MaxValue ? -1 : ret;
     }
 }
+/*
+public class Solution {
+    public int NetworkDelayTime(int[][] times, int N, int K) {
+        Dictionary<int, List<int[]>> graph = new Dictionary<int,List<int[]>>();
+        
+        foreach(int[] path in times){
+            if(!graph.ContainsKey(path[0]))
+                graph.Add(path[0],new List<int[]>());
+            
+            graph[path[0]].Add(new int[]{path[1],path[2]});
+        }
+        
+        Dictionary<int,int> res = new Dictionary<int,int>();
+        
+        SortedSet<int[]> heap = new SortedSet<int[]>(Comparer<int[]>.Create(
+            (a, b) => a[0] == b[0] ? a[1]-b[1]: a[0] - b[0]
+        ));
+        
+        heap.Add(new int[]{0,K});
+        
+        while(heap.Count != 0){
+            int[] path = heap.Min;
+            int cost = path[0];
+            int dest = path[1];
+            heap.Remove(path);
+            
+            if(res.ContainsKey(dest))
+                continue;
+            
+            res.Add(dest,cost);
+            
+            if(graph.ContainsKey(dest)){
+                foreach(int[] edge in graph[dest]){
+                    int nextNode = edge[0];
+                    int dis = edge[1];
+                    if(!res.ContainsKey(nextNode)){
+                        heap.Add(new int[]{cost+dis,nextNode});
+                    }
+                }
+            }
+        }
+        
+        if(res.Count != N)
+            return -1;
+        
+        int max = 0;
+        
+        foreach(KeyValuePair<int, int> length in res){
+            max = Math.Max(max,length.Value);
+        }
+        
+        return max;
+    }
+}
+public class Solution {
+    public int NetworkDelayTime(int[][] times, int N, int K) {
+		--K;
+        int[,] dist = new int[N, N];
+		
+		var inf = (int)1e9;
+		
+		for(var u = 0; u < N; ++u)
+		{
+			for(var v = 0; v < N; ++v)
+			{
+				dist[u, v] = inf;
+			}
+			
+			dist[u, u] = 0;
+		}
+
+		
+		foreach(var e in times)
+		{
+			int u = e[0] - 1, v = e[1] - 1, weight = e[2];
+			dist[u, v] = weight;
+		}
+				
+		for(var k = 0; k < N; ++k)
+			for(var i = 0; i < N; ++i)
+				for(var j = 0; j < N; ++j)
+					if(dist[i, j] > dist[i, k] + dist[k, j])
+						dist[i, j] = dist[i, k] + dist[k, j];
+				
+		var max = 0;
+		for(var u = 0; u < N; ++u)
+			max = Math.Max(max, dist[K, u]);
+		
+		return max == inf ? -1 : max;
+    }
+}
+*/
