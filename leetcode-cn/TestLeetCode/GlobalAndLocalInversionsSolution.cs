6a087d1fb1015df008736eb5683832bcf7277fb9
m15666666 <m15666666@126.com>
2019-08-30 10:00:17 +0800

    775. 全局倒置与局部倒置

--- leetcode-cn/TestLeetCode/GlobalAndLocalInversionsSolution.cs
+++ leetcode-cn/TestLeetCode/GlobalAndLocalInversionsSolution.cs
@@ -4,10 +4,33 @@ using System.Linq;
 using System.Text;
 
 
+/*
+数组 A 是 [0, 1, ..., N - 1] 的一种排列，N 是数组 A 的长度。全局倒置指的是 i,j 满足 0 <= i < j < N 并且 A[i] > A[j] ，局部倒置指的是 i 满足 0 <= i < N 并且 A[i] > A[i+1] 。
+
+当数组 A 中全局倒置的数量等于局部倒置的数量时，返回 true 。
+
+ 
+
+示例 1:
+
+输入: A = [1,0,2]
+输出: true
+解释: 有 1 个全局倒置，和 1 个局部倒置。
+示例 2:
+
+输入: A = [1,2,0]
+输出: false
+解释: 有 2 个全局倒置，和 1 个局部倒置。
+注意:
+
+A 是 [0, 1, ..., A.length - 1] 的一种排列
+A 的长度在 [1, 5000]之间
+这个问题的时间限制已经减少了。
+*/
 /// <summary>
 /// https://leetcode-cn.com/problems/global-and-local-inversions/
 /// 775. 全局倒置与局部倒置
-/// 
+/// https://blog.csdn.net/kongqingxin12/article/details/84748711
 /// </summary>
 class GlobalAndLocalInversionsSolution
 {
@@ -22,6 +45,38 @@ class GlobalAndLocalInversionsSolution
 
     public bool IsIdealPermutation(int[] A)
     {
+        int localCount = 0;
+        int globalCount = 0;
 
+        var upper = A.Length;
+        for ( int i = 1; i < upper; i++)
+        {
+            var v = A[i];
+            if (v < A[i - 1])
+            {
+                localCount++;
+                globalCount++;
+
+                // [2 1 0] 这种情况
+                if (v < i) globalCount += (i - v - 1);
+            }
+            else if(v < i) globalCount += (i - v);// [1 2 0] 这种情况
+        }
+
+        return globalCount == localCount;
     }
 }
+/*
+class Solution {
+public:
+	bool isIdealPermutation(vector<int>& A) 
+	{
+		for (int i = 0; i < A.size(); i++)
+		{
+			if (abs(A[i] - i) > 1)
+				return false;
+		}
+		return true;
+	}
+} 
+*/
---
+++ leetcode-cn/TestLeetCode/SwapAdjacentInLrStringSolution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+在一个由 'L' , 'R' 和 'X' 三个字符组成的字符串（例如"RXXLRXRXL"）中进行移动操作。一次移动操作指用一个"LX"替换一个"XL"，或者用一个"XR"替换一个"RX"。现给定起始字符串start和结束字符串end，请编写代码，当且仅当存在一系列移动操作使得start可以转换成end时， 返回True。
+
+示例 :
+
+输入: start = "RXXLRXRXL", end = "XRLXXRRLX"
+输出: True
+解释:
+我们可以通过以下几步将start转换成end:
+RXXLRXRXL ->
+XRXLRXRXL ->
+XRLXRXRXL ->
+XRLXXRRXL ->
+XRLXXRRLX
+注意:
+
+1 <= len(start) = len(end) <= 10000。
+start和end中的字符串仅限于'L', 'R'和'X'。
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/swap-adjacent-in-lr-string/
+/// 777. 在LR字符串中交换相邻字符
+/// 
+/// </summary>
+class SwapAdjacentInLrStringSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public bool CanTransform(string start, string end)
+    {
+
+    }
+}
