85aa9856ccb2e02faf07b43d6adc956355f38c8a
m15666666 <m15666666@126.com>
2019-09-16 08:55:43 +0800

    808. 分汤

--- leetcode-cn/TestLeetCode/SoupServingsSolution.cs
+++ leetcode-cn/TestLeetCode/SoupServingsSolution.cs
@@ -11,7 +11,8 @@ using System.Text;
 提供 75ml 的汤A 和 25ml 的汤B。
 提供 50ml 的汤A 和 50ml 的汤B。
 提供 25ml 的汤A 和 75ml 的汤B。
-当我们把汤分配给某人之后，汤就没有了。每个回合，我们将从四种概率同为0.25的操作中进行分配选择。如果汤的剩余量不足以完成某次操作，我们将尽可能分配。当两种类型的汤都分配完时，停止操作。
+当我们把汤分配给某人之后，汤就没有了。每个回合，我们将从四种概率同为0.25的操作中进行分配选择。
+如果汤的剩余量不足以完成某次操作，我们将尽可能分配。当两种类型的汤都分配完时，停止操作。
 
 注意不存在先分配100 ml汤B的操作。
 
@@ -31,7 +32,7 @@ using System.Text;
 /// <summary>
 /// https://leetcode-cn.com/problems/soup-servings/
 /// 808. 分汤
-/// 
+/// https://blog.csdn.net/w8253497062015/article/details/80753293
 /// </summary>
 class SoupServingsSolution
 {
@@ -46,6 +47,20 @@ class SoupServingsSolution
 
     public double SoupServings(int N)
     {
+        Dictionary<KeyValuePair<int, int>, double> hash = new Dictionary<KeyValuePair<int, int>, double>();
+        return N > 5000 ? 1.0 : f(N, N, hash);
+    }
 
+    double f(int a, int b, Dictionary<KeyValuePair<int, int>, double> hash)
+    {
+        if (a <= 0 && b <= 0) return 0.5;//A.B同时分配完
+        if (a <= 0) return 1;
+        if (b <= 0) return 0;
+
+        var key = KeyValuePair.Create(a, b);
+        if (hash.ContainsKey(key) && 0 < hash[key]) return hash[key];
+        double possibility = 0.25 * (f(a - 100, b, hash) + f(a - 75, b - 25, hash) + f(a - 50, b - 50, hash) + f(a - 25, b - 75, hash));
+        hash[key] = possibility;
+        return possibility;
     }
 }
