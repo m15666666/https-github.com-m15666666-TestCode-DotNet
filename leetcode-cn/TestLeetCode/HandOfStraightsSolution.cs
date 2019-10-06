d0a0d27f851ce36bcca6b82fca74871844d58e9a
m15666666 <m15666666@126.com>
2019-10-06 10:31:58 +0800

    845. 数组中的最长山脉

---
+++ leetcode-cn/TestLeetCode/HandOfStraightsSolution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+爱丽丝有一手（hand）由整数数组给定的牌。 
+
+现在她想把牌重新排列成组，使得每个组的大小都是 W，且由 W 张连续的牌组成。
+
+如果她可以完成分组就返回 true，否则返回 false。
+
+ 
+
+示例 1：
+
+输入：hand = [1,2,3,6,2,3,4,7,8], W = 3
+输出：true
+解释：爱丽丝的手牌可以被重新排列为 [1,2,3]，[2,3,4]，[6,7,8]。
+示例 2：
+
+输入：hand = [1,2,3,4,5], W = 4
+输出：false
+解释：爱丽丝的手牌无法被重新排列成几个大小为 4 的组。
+ 
+
+提示：
+
+1 <= hand.length <= 10000
+0 <= hand[i] <= 10^9
+1 <= W <= hand.length
+在真实的面试中遇到过这道题？
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/hand-of-straights/
+/// 846. 一手顺子
+/// 
+/// </summary>
+class HandOfStraightsSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public bool IsNStraightHand(int[] hand, int W)
+    {
+
+    }
+}
--- leetcode-cn/TestLeetCode/LongestMountainInArraySolution.cs
+++ leetcode-cn/TestLeetCode/LongestMountainInArraySolution.cs
@@ -32,7 +32,7 @@ B.length >= 3
 提示：
 
 0 <= A.length <= 10000
-0 <= A[i] <= 10000
+0 <= v <= 10000
 */
 /// <summary>
 /// https://leetcode-cn.com/problems/longest-mountain-in-array/
@@ -52,6 +52,52 @@ class LongestMountainInArraySolution
 
     public int LongestMountain(int[] A)
     {
+        if (A == null || A.Length < 3) return 0;
 
+        //标记此时处于上山或下山的任意一中状态（上山为 真 ，反之）
+        //由于第一次必然是上山，所以值为 真
+        bool sign = true;
+        //存储每一次测量的数据
+        int length = 1;
+        //记录截至最新的测量中的最大长度
+        int max = 1;
+        var preV = A[0];
+        int v;
+        //开始“登山”（默认此时处于第一个点）
+        for (int i = 1; i < A.Length; i++, preV = v)
+        {
+            v = A[i];
+            // i - 1 代表当前位置
+            // i  代表眼前位置
+            if (v == preV)//平地，结束登山
+            {
+                sign = true;
+                length = 1;
+                continue;
+            }
+            
+            if (preV < v)
+            {
+                if (sign)//上山中
+                    length++;
+                else // 下山改上山
+                {
+                    sign = true;
+                    length = 2;
+                }
+                continue;
+            }
+
+            //标记此时为下山状态
+            sign = false;
+
+            if (1 < length)//下山中（并且题目要求，最小山脉长度为 3，所以下山之前必须上山一次）
+            {
+                length++;
+                //下山更新最大长度即可
+                max = length > max ? length : max;
+            }
+        }
+        return 2 < max ? max : 0;
     }
 }
