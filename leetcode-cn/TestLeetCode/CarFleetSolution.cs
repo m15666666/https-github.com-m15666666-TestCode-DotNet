144e5f3c26dec92bc8860dc3c8be559af8a30689
m15666666 <m15666666@126.com>
2019-10-10 08:24:24 +0800

    853. 车队

--- leetcode-cn/TestLeetCode/CarFleetSolution.cs
+++ leetcode-cn/TestLeetCode/CarFleetSolution.cs
@@ -59,6 +59,66 @@ class CarFleetSolution
 
     public int CarFleet(int target, int[] position, int[] speed)
     {
+        if (position == null || position.Length == 0) return 0;
+        
+        int len = position.Length;
+        float[] times = new float[len];
+        
+        for ( int i = 0; i < len; ++i )
+            times[i] = ((float)(target - position[i])) / speed[i];
 
+        Array.Sort(position, times);
+
+        int ret = 0;
+        int t = len;
+        for (; 0 < --t;)
+        {
+            if (times[t] < times[t - 1]) ret++; //if cars[t] arrives sooner, it can't be caught
+            else times[t - 1] = times[t]; //else, cars[t-1] arrives at same time as cars[t]
+        }
+        return ret + 1;
+
+        // Car[] cars = new Car[len];
+        //cars[i] = new Car(position[i], ((double)(target - position[i])) / speed[i]);
+
+        //Comparison<Car> comparison = (a, b) => { return a.position.CompareTo(b.position); };
+        //Array.Sort(cars, comparison);
+
+        //int ret = 0;
+        //int t = len;
+        //for ( ; 0 < --t;)
+        //{
+        //    if (cars[t].time < cars[t - 1].time) ret++; //if cars[t] arrives sooner, it can't be caught
+        //    else cars[t - 1] = cars[t]; //else, cars[t-1] arrives at same time as cars[t]
+        //}
+        //return ret + 1;
     }
 }
+/*
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int N = position.length;
+        Car[] cars = new Car[N];
+        for (int i = 0; i < N; ++i)
+            cars[i] = new Car(position[i], (double) (target - position[i]) / speed[i]);
+        Arrays.sort(cars, (a, b) -> Integer.compare(a.position, b.position));
+
+        int ans = 0, t = N;
+        while (--t > 0) {
+            if (cars[t].time < cars[t-1].time) ans++; //if cars[t] arrives sooner, it can't be caught
+            else cars[t-1] = cars[t]; //else, cars[t-1] arrives at same time as cars[t]
+        }
+
+        return ans + (t == 0 ? 1 : 0); //lone car is fleet (if it exists)
+    }
+}
+
+class Car {
+    int position;
+    double time;
+    Car(int p, double t) {
+        position = p;
+        time = t;
+    }
+}
+*/
---
+++ leetcode-cn/TestLeetCode/ExamRoomSolution.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+在考场里，一排有 N 个座位，分别编号为 0, 1, 2, ..., N-1 。
+
+当学生进入考场后，他必须坐在能够使他与离他最近的人之间的距离达到最大化的座位上。如果有多个这样的座位，他会坐在编号最小的座位上。(另外，如果考场里没有人，那么学生就坐在 0 号座位上。)
+
+返回 ExamRoom(int N) 类，它有两个公开的函数：其中，函数 ExamRoom.seat() 会返回一个 int （整型数据），代表学生坐的位置；函数 ExamRoom.leave(int p) 代表坐在座位 p 上的学生现在离开了考场。每次调用 ExamRoom.leave(p) 时都保证有学生坐在座位 p 上。
+
+ 
+
+示例：
+
+输入：["ExamRoom","seat","seat","seat","seat","leave","seat"], [[10],[],[],[],[],[4],[]]
+输出：[null,0,9,4,2,null,5]
+解释：
+ExamRoom(10) -> null
+seat() -> 0，没有人在考场里，那么学生坐在 0 号座位上。
+seat() -> 9，学生最后坐在 9 号座位上。
+seat() -> 4，学生最后坐在 4 号座位上。
+seat() -> 2，学生最后坐在 2 号座位上。
+leave(4) -> null
+seat() -> 5，学生最后坐在 5 号座位上。
+ 
+
+提示：
+
+1 <= N <= 10^9
+在所有的测试样例中 ExamRoom.seat() 和 ExamRoom.leave() 最多被调用 10^4 次。
+保证在调用 ExamRoom.leave(p) 时有学生正坐在座位 p 上。
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/exam-room/
+/// 855. 考场就座
+/// 
+/// </summary>
+class ExamRoomSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    
+    public int ClimbStairs(int n)
+    {
+        Dictionary<int, int> n2PathCount = new Dictionary<int, int>();
+        return ClimbStairs(n, n2PathCount);
+    }
+
+    private int ClimbStairs(int n, Dictionary<int, int> n2PathCount)
+    {
+        if (n <= 1) return 1;
+        if (n == 2) return 2;
+
+        if (n2PathCount.ContainsKey(n)) return n2PathCount[n];
+
+        var ret = ClimbStairs(n - 1, n2PathCount) + ClimbStairs(n - 2, n2PathCount);
+
+        if (!n2PathCount.ContainsKey(n)) n2PathCount[n] = ret;
+
+        return ret;
+    }
+}
