b91aeac127ba1a3861431891692688896df575ef
m15666666 <m15666666@126.com>
2019-11-02 08:41:46 +0800

    904. 水果成篮

---
+++ leetcode-cn/TestLeetCode/SumOfSubarrayMinimumsSolution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+给定一个整数数组 A，找到 min(B) 的总和，其中 B 的范围为 A 的每个（连续）子数组。
+
+由于答案可能很大，因此返回答案模 10^9 + 7。
+
+ 
+
+示例：
+
+输入：[3,1,2,4]
+输出：17
+解释：
+子数组为 [3]，[1]，[2]，[4]，[3,1]，[1,2]，[2,4]，[3,1,2]，[1,2,4]，[3,1,2,4]。 
+最小值为 3，1，2，4，1，1，2，1，1，1，和为 17。
+ 
+
+提示：
+
+1 <= A <= 30000
+1 <= A[i] <= 30000
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/sum-of-subarray-minimums/
+/// 907. 子数组的最小值之和
+/// 
+/// </summary>
+class SumOfSubarrayMinimumsSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public int SumSubarrayMins(int[] A)
+    {
+
+    }
+}
