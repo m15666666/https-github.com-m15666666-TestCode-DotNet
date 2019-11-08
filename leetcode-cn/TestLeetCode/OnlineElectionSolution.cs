c4d3360606f46c42c401ec1d1ca69570d8be07a1
m15666666 <m15666666@126.com>
2019-11-08 12:47:06 +0800

    911. 在线选举

--- leetcode-cn/TestLeetCode/OnlineElectionSolution.cs
+++ leetcode-cn/TestLeetCode/OnlineElectionSolution.cs
@@ -48,11 +48,181 @@ class OnlineElectionSolution
 
     public OnlineElectionSolution(int[] persons, int[] times)
     {
+        var count = new Dictionary<int,int>();
+        for (int i = 0; i < persons.Length; ++i)
+        {
+            int p = persons[i], t = times[i];
+            if (!count.ContainsKey(p)) count.Add(p, 1);
+            else count[p]++;
+            int c = count[p];
 
+            while (A.Count <= c)
+                A.Add(new List<Vote>());
+            A[c].Add(new Vote(p, t));
+        }
     }
 
     public int Q(int t)
     {
+        // Binary search on A[i][0].time for smallest i
+        // such that A[i][0].time > t
+        int lo = 1, hi = A.Count;
+        while (lo < hi)
+        {
+            int mi = lo + (hi - lo) / 2;
+            if (A[mi][0].time <= t) lo = mi + 1;
+            else hi = mi;
+        }
+        int i = lo - 1;
 
+        // Binary search on A[i][j].time for smallest j
+        // such that A[i][j].time > t
+        lo = 0; hi = A[i].Count;
+        while (lo < hi)
+        {
+            int mi = lo + (hi - lo) / 2;
+            if (A[i][mi].time <= t)
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+        int j = Math.Max(lo - 1, 0);
+        return A[i][j].person;
+    }
+
+    private List<List<Vote>> A = new List<List<Vote>>();
+
+    public class Vote
+    {
+        public int person;
+        public int time;
+        public Vote(int p, int t)
+        {
+            person = p;
+            time = t;
+        }
     }
 }
+/*
+方法 1：列表 + 二分搜索
+想法和算法
+
+我么可以把选票存储在选票列表的列表 A 中。每个投票都有一个人和一个时间戳，A[count] 是一个列表，记录当前人获得的第 count 张选票。
+
+然后，A[i][0] 和 A[i] 单调增加，所以我们可以利用二分搜索根据时间找到最近的选票。
+
+JavaPython
+class TopVotedCandidate {
+    List<List<Vote>> A;
+    public TopVotedCandidate(int[] persons, int[] times) {
+        A = new ArrayList();
+        Map<Integer, Integer> count = new HashMap();
+        for (int i = 0; i < persons.length; ++i) {
+            int p = persons[i], t = times[i];
+            int c = count.getOrDefault(p, 0) + 1;
+
+            count.put(p, c);
+            while (A.size() <= c)
+                A.add(new ArrayList<Vote>());
+            A.get(c).add(new Vote(p, t));
+        }
+    }
+
+    public int q(int t) {
+        // Binary search on A[i][0].time for smallest i
+        // such that A[i][0].time > t
+        int lo = 1, hi = A.size();
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (A.get(mi).get(0).time <= t)
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+        int i = lo - 1;
+
+        // Binary search on A[i][j].time for smallest j
+        // such that A[i][j].time > t
+        lo = 0; hi = A.get(i).size();
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (A.get(i).get(mi).time <= t)
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+        int j = Math.max(lo-1, 0);
+        return A.get(i).get(j).person;
+    }
+}
+
+class Vote {
+    int person, time;
+    Vote(int p, int t) {
+        person = p;
+        time = t;
+    }
+}
+复杂度分析
+
+时间复杂度：O(N + Q \log^2 N)O(N+Qlog 
+2
+ N)，其中 NN 是选票的个数，QQ 是询问的个数。
+空间复杂度：O(N)O(N)。
+方法 2：预计算结果 + 二分搜索
+想法和算法
+
+每当选票记录，我们可以记录每个胜者改变的 (winner, time) 时刻信息。之后，我们拥有一个有序的时刻信息，并用二分搜索找到答案。
+
+JavaPython
+class TopVotedCandidate {
+    List<Vote> A;
+    public TopVotedCandidate(int[] persons, int[] times) {
+        A = new ArrayList();
+        Map<Integer, Integer> count = new HashMap();
+        int leader = -1;  // current leader
+        int m = 0;  // current number of votes for leader
+
+        for (int i = 0; i < persons.length; ++i) {
+            int p = persons[i], t = times[i];
+            int c = count.getOrDefault(p, 0) + 1;
+            count.put(p, c);
+
+            if (c >= m) {
+                if (p != leader) {  // lead change
+                    leader = p;
+                    A.add(new Vote(leader, t));
+                }
+
+                if (c > m) m = c;
+            }
+        }
+    }
+
+    public int q(int t) {
+        int lo = 1, hi = A.size();
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (A.get(mi).time <= t)
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+
+        return A.get(lo - 1).person;
+    }
+}
+
+class Vote {
+    int person, time;
+    Vote(int p, int t) {
+        person = p;
+        time = t;
+    }
+}
+复杂度分析
+
+时间复杂度：O(N + Q\log{N})O(N+QlogN)，其中 NN 是选票个数，QQ 是询问个数。
+空间复杂度：O(N)O(N)。
+ 
+*/
---
+++ leetcode-cn/TestLeetCode/SortAnArraySolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+给定一个整数数组 nums，将该数组升序排列。
+
+ 
+
+示例 1：
+
+输入：[5,2,3,1]
+输出：[1,2,3,5]
+示例 2：
+
+输入：[5,1,1,2,0,0]
+输出：[0,0,1,1,2,5]
+ 
+
+提示：
+
+1 <= A.length <= 10000
+-50000 <= A[i] <= 50000
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/sort-an-array/
+/// 912. 排序数组
+/// 
+/// </summary>
+class SortAnArraySolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public IList<int> SortArray(int[] nums)
+    {
+
+    }
+}
