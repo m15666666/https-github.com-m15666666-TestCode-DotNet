9061873571ffdcce99953de9ec4bcfcaf6069543
m15666666 <m15666666@126.com>
2019-11-15 08:02:54 +0800

    921. 使括号有效的最少添加

--- leetcode-cn/TestLeetCode/MinimumAddToMakeParenthesesValidSolution.cs
+++ leetcode-cn/TestLeetCode/MinimumAddToMakeParenthesesValidSolution.cs
@@ -57,6 +57,68 @@ class MinimumAddToMakeParenthesesValidSolution
 
     public int MinAddToMakeValid(string S)
     {
+        int ret = 0;
+        int balance = 0;
+        foreach (var c in S)
+        {
+            balance += c == '(' ? 1 : -1;
+            if (balance == -1)
+            {
+                ret++;
+                balance++;
+            }
+        }
 
+        return ret + balance;
     }
 }
+/*
+方法一： 平衡法
+思路和算法
+
+保证左右括号数量的 平衡： 计算 '(' 出现的次数减去 ')' 出现的次数。如果值为 0，那就是平衡的，如果小于 0，就要在前面补上缺少的 '('。
+
+计算 S 每个前缀子数组的 平衡度。如果值是负数（比如说，-1），那就得在前面加上一个 '('。同样的，如果值是正数（比如说，+B），那就得在末尾处加上 B 个 ')' 。
+
+JavaPython
+class Solution {
+    public int minAddToMakeValid(String S) {
+        int ans = 0, bal = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            bal += S.charAt(i) == '(' ? 1 : -1;
+            // It is guaranteed bal >= -1
+            if (bal == -1) {
+                ans++;
+                bal++;
+            }
+        }
+
+        return ans + bal;
+    }
+}
+复杂度分析
+
+时间复杂度： O(N)O(N)，其中 NN 是 S 的长度。
+
+空间复杂度： O(1)O(1)。
+
+
+public class Solution {
+    public int MinAddToMakeValid(string S) {
+        var stack = new Stack<char>();
+        int right = 0;
+        foreach(char item in S){
+            if(item == '(')
+                stack.Push(item);
+            else {
+                if(stack.Count != 0)
+                    stack.Pop();
+                else
+                    right++;
+            }
+        }        
+            
+        return right + stack.Count;
+    }
+} 
+*/
---
+++ leetcode-cn/TestLeetCode/ThreesumWithMultiplicitySolution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+给定一个整数数组 A，以及一个整数 target 作为目标值，返回满足 i < j < k 且 A[i] + A[j] + A[k] == target 的元组 i, j, k 的数量。
+
+由于结果会非常大，请返回 结果除以 10^9 + 7 的余数。
+
+示例 1：
+
+输入：A = [1,1,2,2,3,3,4,4,5,5], target = 8
+输出：20
+解释：
+按值枚举（A[i]，A[j]，A[k]）：
+(1, 2, 5) 出现 8 次；
+(1, 3, 4) 出现 8 次；
+(2, 2, 4) 出现 2 次；
+(2, 3, 3) 出现 2 次。
+示例 2：
+
+输入：A = [1,1,2,2,2,2], target = 5
+输出：12
+解释：
+A[i] = 1，A[j] = A[k] = 2 出现 12 次：
+我们从 [1,1] 中选择一个 1，有 2 种情况，
+从 [2,2,2,2] 中选出两个 2，有 6 种情况。
+ 
+
+提示：
+
+3 <= A.length <= 3000
+0 <= A[i] <= 100
+0 <= target <= 300
+在真实的面试中遇到过这道题？
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/3sum-with-multiplicity/
+/// 923. 三数之和的多种可能
+/// 
+/// </summary>
+class ThreesumWithMultiplicitySolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public int ThreeSumMulti(int[] A, int target)
+    {
+
+    }
+}
