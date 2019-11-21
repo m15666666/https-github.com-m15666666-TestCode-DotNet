c76564164d9145d12fa4266f91073543ec048b50
m15666666 <m15666666@126.com>
2019-11-21 08:27:51 +0800

    931. 下降路径最小和

---
+++ leetcode-cn/TestLeetCode/BeautifulArraySolution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+对于某些固定的 N，如果数组 A 是整数 1, 2, ..., N 组成的排列，使得：
+
+对于每个 i < j，都不存在 k 满足 i < k < j 使得 A[k] * 2 = A[i] + A[j]。
+
+那么数组 A 是漂亮数组。
+
+给定 N，返回任意漂亮数组 A（保证存在一个）。
+
+示例 1：
+
+输入：4
+输出：[2,1,4,3]
+示例 2：
+
+输入：5
+输出：[3,1,2,5,4]
+
+提示：
+
+1 <= N <= 1000
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/beautiful-array/
+/// 932. 漂亮数组
+/// 
+/// </summary>
+class BeautifulArraySolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public int[] BeautifulArray(int N)
+    {
+
+    }
+}
