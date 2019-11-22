4dc21110174bf9a5bb925ea66e47c1699b8a14b3
m15666666 <m15666666@126.com>
2019-11-22 08:36:35 +0800

    932. 漂亮数组

---
+++ leetcode-cn/TestLeetCode/ShortestBridgeSolution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+在给定的二维二进制数组 A 中，存在两座岛。（岛是由四面相连的 1 形成的一个最大组。）
+
+现在，我们可以将 0 变为 1，以使两座岛连接起来，变成一座岛。
+
+返回必须翻转的 0 的最小数目。（可以保证答案至少是 1。）
+
+示例 1：
+
+输入：[[0,1],[1,0]]
+输出：1
+示例 2：
+
+输入：[[0,1,0],[0,0,0],[0,0,1]]
+输出：2
+示例 3：
+
+输入：[[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]
+输出：1
+
+提示：
+
+1 <= A.length = A[0].length <= 100
+A[i][j] == 0 或 A[i][j] == 1
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/shortest-bridge/
+/// 934. 最短的桥
+/// 
+/// </summary>
+class ShortestBridgeSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public int ShortestBridge(int[][] A)
+    {
+
+    }
+}
