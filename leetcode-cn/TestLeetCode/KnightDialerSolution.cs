f02456b24f5b3765941ac77118c015fca357735f
m15666666 <m15666666@126.com>
2019-11-25 15:33:00 +0800

    934. 最短的桥 935. 骑士拨号器

---
+++ leetcode-cn/TestLeetCode/KnightDialerSolution.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+国际象棋中的骑士可以按下图所示进行移动：
+
+ .           
+
+
+这一次，我们将 “骑士” 放在电话拨号盘的任意数字键（如上图所示）上，接下来，骑士将会跳 N-1 步。每一步必须是从一个数字键跳到另一个数字键。
+
+每当它落在一个键上（包括骑士的初始位置），都会拨出键所对应的数字，总共按下 N 位数字。
+
+你能用这种方式拨出多少个不同的号码？
+
+因为答案可能很大，所以输出答案模 10^9 + 7。
+
+ 
+
+示例 1：
+
+输入：1
+输出：10
+示例 2：
+
+输入：2
+输出：20
+示例 3：
+
+输入：3
+输出：46
+ 
+
+提示：
+
+1 <= N <= 5000
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/knight-dialer/
+/// 935. 骑士拨号器
+/// 
+/// </summary>
+class KnightDialerSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public int KnightDialer(int N)
+    {
+        const int MOD = 1_000_000_007;
+
+        var current = new int[10];
+        var previous = new int[10];
+        Array.Fill(current, 1);
+        
+        for( int step = N - 2; -1 < step; step--)
+        {
+            Array.Fill(previous, 0);
+
+            for (int node = 0; node < 10; ++node)
+                foreach (int nextMove in NextMoves[node])
+                {
+                    previous[nextMove] = (current[node] + previous[nextMove]) % MOD;
+                }
+
+            (current, previous) = (previous, current);
+        }
+
+        long ret = 0;
+        foreach (int x in current)
+            ret += x;
+        return (int)(ret % MOD);
+    }
+
+    private static readonly int[][] NextMoves = new int[][]{
+            new []{4,6},// 起点是：0
+            new []{6,8},//1
+            new []{7,9},//2
+            new []{4,8},//3
+            new []{3,9,0},//4
+            new int[0],//5 跳不了
+            new []{1,7,0},//6
+            new []{2,6},//7
+            new []{1,3},//8
+            new []{2,4}//9
+        };
+}
+/*
+方法一：动态规划
+我们用 f(start, n) 表示骑士从数字 start 开始，跳了 n - 1 步得到不同的 n 位数字的个数。f(start, n) 可以从 f(x, n - 1) 转移而来，其中 x 是任意一个可以一步跳到 start 的数字。例如当 start = 1，时，x 可以为 6 或 8，因此有 f(1, n) = f(6, n - 1) + f(8, n - 1)。
+
+最终的答案即为 f(0, N) + f(1, N) + ... + f(9, N)。我们可以使用滚动数组减少空间复杂度，这是因为 f(start, n) 只和 f(x, n - 1) 有关，因此在计算 f(start, n) 时，所有第二维小于 n - 1 的 f 值都不必存储。也就是说，我们只要实时存储当前正在计算的所有 f 值（n 位数字）以及上一个状态的 f 值（n - 1 位数字）即可。在 Java 代码中，我们使用 dp[0][start] 和 dp[1][start] 交替表示当前和上一个状态的 f 值。在 Python 代码中，我们使用 dp2 数组计算出当前的 f 值后，直接覆盖存储了上一个状态的 f 值的 dp 数组。
+
+JavaPython
+class Solution {
+    public int knightDialer(int N) {
+        int MOD = 1_000_000_007;
+        int[][] moves = new int[][]{
+            {4,6},{6,8},{7,9},{4,8},{3,9,0},
+            {},{1,7,0},{2,6},{1,3},{2,4}};
+
+        int[][] dp = new int[2][10];
+        Arrays.fill(dp[0], 1);
+        for (int hops = 0; hops < N-1; ++hops) {
+            Arrays.fill(dp[~hops & 1], 0);
+            for (int node = 0; node < 10; ++node)
+                for (int nei: moves[node]) {
+                    dp[~hops & 1][nei] += dp[hops & 1][node];
+                    dp[~hops & 1][nei] %= MOD;
+                }
+        }
+
+        long ans = 0;
+        for (int x: dp[~N & 1])
+            ans += x;
+        return (int) (ans % MOD);
+    }
+}
+复杂度分析
+
+时间复杂度：O(N)O(N)。
+
+空间复杂度：如果使用滚动数组，则空间复杂度为 O(1)O(1)，也可以看成 O(10)O(10)。否则空间复杂度为 O(N)O(N)。 
+     
+     
+*/
--- leetcode-cn/TestLeetCode/ShortestBridgeSolution.cs
+++ leetcode-cn/TestLeetCode/ShortestBridgeSolution.cs
@@ -26,7 +26,7 @@ using System.Text;
 
 提示：
 
-1 <= A.length = A[0].length <= 100
+1 <= A.Length = A[0].Length <= 100
 A[i][j] == 0 或 A[i][j] == 1
 */
 /// <summary>
@@ -47,6 +47,201 @@ class ShortestBridgeSolution
 
     public int ShortestBridge(int[][] A)
     {
+        int R = A.Length, C = A[0].Length;
+        int[,] colors = GetColors(A);
 
+        Queue<Node> queue = new Queue<Node>();
+        var target = new HashSet<int>();
+
+        for (int r = 0; r < R; ++r)
+            for (int c = 0; c < C; ++c)
+            {
+                if (colors[r,c] == 1)
+                {
+                    queue.Enqueue(new Node(r, c, 0));
+                }
+                else if (colors[r,c] == 2)
+                {
+                    target.Add(r * C + c);
+                }
+            }
+
+        while (0 < queue.Count)
+        {
+            Node node = queue.Dequeue();
+            if (target.Contains(node.r * C + node.c)) return node.depth - 1;
+
+            foreach (int nei in GetNeighbors(A, node.r, node.c))
+            {
+                int nr = nei / C, nc = nei % C;
+                if (colors[nr,nc] != 1)
+                {
+                    queue.Enqueue(new Node(nr, nc, node.depth + 1));
+                    colors[nr,nc] = 1;
+                }
+            }
+        }
+        return R + C;
+    }
+
+    private static int[,] GetColors(int[][] A)
+    {
+        int R = A.Length;
+        int C = A[0].Length;
+        int[,] colors = new int[R,C];
+        int t = 0;
+
+        for (int r0 = 0; r0 < R; ++r0)
+            for (int c0 = 0; c0 < C; ++c0)
+                if (colors[r0,c0] == 0 && A[r0][c0] == 1)
+                {
+                    // Start dfs
+                    Stack<int> stack = new Stack<int>();
+                    stack.Push(r0 * C + c0);
+                    colors[r0,c0] = ++t;
+
+                    while (0 < stack.Count)
+                    {
+                        int node = stack.Pop();
+                        int r = node / C, c = node % C;
+                        foreach (int nei in GetNeighbors(A, r, c))
+                        {
+                            int nr = nei / C, nc = nei % C;
+                            if (A[nr][nc] == 1 && colors[nr,nc] == 0)
+                            {
+                                colors[nr,nc] = t;
+                                stack.Push(nr * C + nc);
+                            }
+                        }
+                    }
+                }
+
+        return colors;
+    }
+
+    private static List<int> GetNeighbors(int[][] A, int r, int c)
+    {
+        int R = A.Length, C = A[0].Length;
+        List<int> ans = new List<int>();
+        if (0 <= r - 1) ans.Add((r - 1) * R + c);
+        if (0 <= c - 1) ans.Add(r * R + (c - 1));
+        if (r + 1 < R) ans.Add((r + 1) * R + c);
+        if (c + 1 < C) ans.Add(r * R + (c + 1));
+        return ans;
+    }
+
+    public class Node
+    {
+        public int r;
+        public int c;
+        public int depth;
+        public Node(int r, int c, int d)
+        {
+            this.r = r;
+            this.c = c;
+            depth = d;
+        }
     }
 }
+/*
+方法一：搜索
+分析
+
+我们使用的方法非常直接：首先找到这两座岛，随后选择一座，将它不断向外延伸一圈，直到到达了另一座岛。
+
+在寻找这两座岛时，我们使用深度优先搜索。在向外延伸时，我们使用广度优先搜索。
+
+算法
+
+我们通过对数组 A 中的 1 进行深度优先搜索，可以得到两座岛的位置集合，分别为 source 和 target。随后我们从 source 中的所有位置开始进行广度优先搜索，当它们到达了 target 中的任意一个位置时，搜索的层数就是答案。
+
+JavaPython
+class Solution {
+    public int shortestBridge(int[][] A) {
+        int R = A.length, C = A[0].length;
+        int[][] colors = getComponents(A);
+
+        Queue<Node> queue = new LinkedList();
+        Set<Integer> target = new HashSet();
+
+        for (int r = 0; r < R; ++r)
+            for (int c = 0; c < C; ++c) {
+                if (colors[r][c] == 1) {
+                    queue.add(new Node(r, c, 0));
+                } else if (colors[r][c] == 2) {
+                    target.add(r * C + c);
+                }
+            }
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (target.contains(node.r * C + node.c))
+                return node.depth - 1;
+            for (int nei: neighbors(A, node.r, node.c)) {
+                int nr = nei / C, nc = nei % C;
+                if (colors[nr][nc] != 1) {
+                    queue.add(new Node(nr, nc, node.depth + 1));
+                    colors[nr][nc] = 1;
+                }
+            }
+        }
+
+        throw null;
+    }
+
+    public int[][] getComponents(int[][] A) {
+        int R = A.length, C = A[0].length;
+        int[][] colors = new int[R][C];
+        int t = 0;
+
+        for (int r0 = 0; r0 < R; ++r0)
+            for (int c0 = 0; c0 < C; ++c0)
+                if (colors[r0][c0] == 0 && A[r0][c0] == 1) {
+                    // Start dfs
+                    Stack<Integer> stack = new Stack();
+                    stack.push(r0 * C + c0);
+                    colors[r0][c0] = ++t;
+
+                    while (!stack.isEmpty()) {
+                        int node = stack.pop();
+                        int r = node / C, c = node % C;
+                        for (int nei: neighbors(A, r, c)) {
+                            int nr = nei / C, nc = nei % C;
+                            if (A[nr][nc] == 1 && colors[nr][nc] == 0) {
+                                colors[nr][nc] = t;
+                                stack.push(nr * C + nc);
+                            }
+                        }
+                    }
+                }
+
+        return colors;
+    }
+
+    public List<Integer> neighbors(int[][] A, int r, int c) {
+        int R = A.length, C = A[0].length;
+        List<Integer> ans = new ArrayList();
+        if (0 <= r-1) ans.add((r-1) * R + c);
+        if (0 <= c-1) ans.add(r * R + (c-1));
+        if (r+1 < R) ans.add((r+1) * R + c);
+        if (c+1 < C) ans.add(r * R + (c+1));
+        return ans;
+    }
+}
+
+class Node {
+    int r, c, depth;
+    Node(int r, int c, int d) {
+        this.r = r;
+        this.c = c;
+        depth = d;
+    }
+}
+复杂度分析
+
+时间复杂度：O(MN)O(MN)，其中 MM 和 NN 分别是数组 A 的行数和列数。
+
+空间复杂度：O(MN)O(MN)。
+
+ 
+*/
