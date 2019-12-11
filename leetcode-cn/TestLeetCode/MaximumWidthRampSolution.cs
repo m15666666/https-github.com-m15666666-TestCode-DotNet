6ac012d016776a871b60c1141dd8cf12a7dcc6f2
m15666666 <m15666666@126.com>
2019-12-11 12:18:33 +0800

    959. 由斜杠划分区域

---
+++ leetcode-cn/TestLeetCode/MaximumWidthRampSolution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+给定一个整数数组 A，坡是元组 (i, j)，其中  i < j 且 A[i] <= A[j]。这样的坡的宽度为 j - i。
+
+找出 A 中的坡的最大宽度，如果不存在，返回 0 。
+
+ 
+
+示例 1：
+
+输入：[6,0,8,2,1,5]
+输出：4
+解释：
+最大宽度的坡为 (i, j) = (1, 5): A[1] = 0 且 A[5] = 5.
+示例 2：
+
+输入：[9,8,1,0,1,9,4,0,4,1]
+输出：7
+解释：
+最大宽度的坡为 (i, j) = (2, 9): A[2] = 1 且 A[9] = 1.
+ 
+
+提示：
+
+2 <= A.length <= 50000
+0 <= A[i] <= 50000
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/maximum-width-ramp/
+/// 962. 最大宽度坡
+/// 
+/// </summary>
+class MaximumWidthRampSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public int MaxWidthRamp(int[] A)
+    {
+
+    }
+}
