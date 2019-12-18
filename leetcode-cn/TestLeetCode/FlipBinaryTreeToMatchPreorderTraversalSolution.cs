860a7fe252a998d92b34ee16da8d7d36fb2bc747
m15666666 <m15666666@126.com>
2019-12-18 09:19:29 +0800

    969. 煎饼排序

---
+++ leetcode-cn/TestLeetCode/FlipBinaryTreeToMatchPreorderTraversalSolution.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+给定一个有 N 个节点的二叉树，每个节点都有一个不同于其他节点且处于 {1, ..., N} 中的值。
+
+通过交换节点的左子节点和右子节点，可以翻转该二叉树中的节点。
+
+考虑从根节点开始的先序遍历报告的 N 值序列。将这一 N 值序列称为树的行程。
+
+（回想一下，节点的先序遍历意味着我们报告当前节点的值，然后先序遍历左子节点，再先序遍历右子节点。）
+
+我们的目标是翻转最少的树中节点，以便树的行程与给定的行程 voyage 相匹配。 
+
+如果可以，则返回翻转的所有节点的值的列表。你可以按任何顺序返回答案。
+
+如果不能，则返回列表 [-1]。
+
+ 
+
+示例 1：
+
+
+
+输入：root = [1,2], voyage = [2,1]
+输出：[-1]
+示例 2：
+
+
+
+输入：root = [1,2,3], voyage = [1,3,2]
+输出：[1]
+示例 3：
+
+
+
+输入：root = [1,2,3], voyage = [1,2,3]
+输出：[]
+ 
+
+提示：
+
+1 <= N <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/flip-binary-tree-to-match-preorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。给定一个有 N 个节点的二叉树，每个节点都有一个不同于其他节点且处于 {1, ..., N} 中的值。
+
+通过交换节点的左子节点和右子节点，可以翻转该二叉树中的节点。
+
+考虑从根节点开始的先序遍历报告的 N 值序列。将这一 N 值序列称为树的行程。
+
+（回想一下，节点的先序遍历意味着我们报告当前节点的值，然后先序遍历左子节点，再先序遍历右子节点。）
+
+我们的目标是翻转最少的树中节点，以便树的行程与给定的行程 voyage 相匹配。 
+
+如果可以，则返回翻转的所有节点的值的列表。你可以按任何顺序返回答案。
+
+如果不能，则返回列表 [-1]。
+
+ 
+
+示例 1：
+
+
+
+输入：root = [1,2], voyage = [2,1]
+输出：[-1]
+示例 2：
+
+
+
+输入：root = [1,2,3], voyage = [1,3,2]
+输出：[1]
+示例 3：
+
+
+
+输入：root = [1,2,3], voyage = [1,2,3]
+输出：[]
+ 
+
+提示：
+
+1 <= N <= 100
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/flip-binary-tree-to-match-preorder-traversal/
+/// 971. 翻转二叉树以匹配先序遍历
+/// 
+/// </summary>
+class FlipBinaryTreeToMatchPreorderTraversalSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public IList<int> FlipMatchVoyage(TreeNode root, int[] voyage)
+    {
+
+    }
+}
--- leetcode-cn/TestLeetCode/PancakeSortingSolution.cs
+++ leetcode-cn/TestLeetCode/PancakeSortingSolution.cs
@@ -43,7 +43,8 @@ class PancakeSortingSolution
 {
     public void Test()
     {
-        //int[] nums = new int[] {3, 2, 4};
+        int[] nums = new int[] {3, 2, 4,1};
+        var ret = PancakeSort(nums);
         //int k = 6;
         //var ret = LevelOrder((int[]) nums.Clone(), k);
 
@@ -52,6 +53,98 @@ class PancakeSortingSolution
 
     public IList<int> PancakeSort(int[] A)
     {
+        int len = A.Length;
 
+        var indexes = Enumerable.Range(1, len).ToArray();
+        Comparison<int> comparison = (i1, i2) => A[i2-1].CompareTo(A[i1-1]); // 按照数值倒序排列
+        Array.Sort( indexes, comparison);
+
+        List<int> ret = new List<int>();
+        foreach (int i in indexes)
+        {
+            int index = i;
+            foreach (int windows in ret)
+                if (index <= windows) index = windows + 1 - index;
+
+            ret.Add(index); ;
+            ret.Add(len--);
+        }
+
+        return ret;
     }
 }
+/*
+方法：从大到小排序
+思路
+
+我们可以将最大的元素（在位置 i，下标从 1 开始）进行煎饼翻转 i 操作将它移动到序列的最前面，然后再使用煎饼翻转 A.length 操作将它移动到序列的最后面。 此时，最大的元素已经移动到正确的位置上了，所以之后我们就不需要再使用 k 值大于等于 A.length 的煎饼翻转操作了。
+
+我们可以重复这个过程直到序列被排好序为止。 每一步，我们只需要花费两次煎饼翻转操作。
+
+算法
+
+我们从数组 A 中的最大值向最小值依次进行枚举，每一次将枚举的元素放到正确的位置上。
+
+每一步，对于在位置 i 的（从大到小枚举的）元素，我们会使用思路中提到的煎饼翻转组合操作将它移动到正确的位置上。 值得注意的是，执行一次煎饼翻转操作 f，会将位置在 i, i <= f 的元素翻转到位置 f+1 - i 上。
+
+JavaPython
+class Solution {
+    public List<Integer> pancakeSort(int[] A) {
+        List<Integer> ans = new ArrayList();
+        int N = A.length;
+
+        Integer[] B = new Integer[N];
+        for (int i = 0; i < N; ++i)
+            B[i] = i+1;
+        Arrays.sort(B, (i, j) -> A[j-1] - A[i-1]);
+
+        for (int i: B) {
+            for (int f: ans)
+                if (i <= f)
+                    i = f+1 - i;
+            ans.add(i);
+            ans.add(N--);
+        }
+
+        return ans;
+    }
+}
+复杂度分析
+
+时间复杂度：O(N^2)O(N 
+2
+ )，其中 NN 是数组 A 的长度。
+
+空间复杂度：O(N)O(N)。
+
+
+public class Solution {
+    public IList<int> PancakeSort(int[] A) {
+        List<int> ans = new List<int>();
+        
+        for(int i = A.Length;i>0;i--){
+            int j = 0;
+            while(A[j] != i)
+                j++;
+            reverse(A,j+1);
+            ans.Add(j+1);
+            reverse(A,i);
+            ans.Add(i);
+        }
+        return ans;
+    }
+    
+    public void reverse(int[] A, int k){
+        int l = 0;
+        int r = k-1;
+        
+        while(l <r){
+            int temp = A[l];
+            A[l] = A[r];
+            A[r] = temp;
+            l++;
+            r--;
+        }
+    }
+} 
+*/
