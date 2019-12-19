b205710ec77c4bb2225fd7273eed09b4bb7037ac
m15666666 <m15666666@126.com>
2019-12-19 16:26:47 +0800

    Moons.ExportData2Center.Client

---
+++ Moons.ExportData2Center.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Moons.ExportData2Center.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Moons.ExportData2Center.Client");
+
+            string conn = "server=.;database=jg2642empty;uid=sa;pwd=#db877350;";
+            string sql = "SELECT TOP 10 * FROM Pnt_Point where Point_ID > 1000399 order by Point_ID;";
+            using (IDbConnection connection = new SqlConnection(conn))
+            {
+                var datas = connection.Query<dynamic>(sql).ToList();
+                //var datas = connection.Query<object>(sql).ToList();
+                //var datas = connection.Query(sql).ToList();
+                foreach ( var data in datas)
+                {
+                    var json = JsonConvert.SerializeObject(data);
+
+                    var o2 = JsonConvert.DeserializeObject(json);
+                }
+                //var jsons = JsonConvert.SerializeObject(datas);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
