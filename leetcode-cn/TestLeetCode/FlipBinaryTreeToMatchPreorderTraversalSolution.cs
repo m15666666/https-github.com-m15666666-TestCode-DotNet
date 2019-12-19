4283214a8e9b07b74ebe063921a1140a990e360b
m15666666 <m15666666@126.com>
2019-12-19 09:44:50 +0800

    971. 翻转二叉树以匹配先序遍历

--- leetcode-cn/TestLeetCode/FlipBinaryTreeToMatchPreorderTraversalSolution.cs
+++ leetcode-cn/TestLeetCode/FlipBinaryTreeToMatchPreorderTraversalSolution.cs
@@ -105,6 +105,128 @@ class FlipBinaryTreeToMatchPreorderTraversalSolution
 
     public IList<int> FlipMatchVoyage(TreeNode root, int[] voyage)
     {
+        flipped = new List<int>();
+        index = 0;
+        this.voyage = voyage;
+        isFailed = false;
 
+        Dfs(root);
+
+        if (isFailed) return new int[] { -1 };
+
+        return flipped;
+    }
+
+    private List<int> flipped;
+    private int index = 0;
+    private int[] voyage;
+    private bool isFailed;
+
+    private void Dfs(TreeNode node)
+    {
+        if (isFailed || node == null) return;
+
+        if (node.val != voyage[index++])
+        {
+            isFailed = true;
+            return;
+        }
+
+        var left = node.left;
+        var right = node.right;
+        if (left == null)
+        {
+            if (right == null) return;
+            Dfs(right);
+        }
+        else
+        {
+            if (left.val != voyage[index])
+            {
+                if (right == null)
+                {
+                    isFailed = true;
+                    return;
+                }
+                flipped.Add(node.val);
+                (left, right) = (right, left);
+            }
+            Dfs(left);
+            Dfs(right);
+        }
+
+
+        //if (node.left != null && node.left.val != voyage[index])
+        //{
+        //    flipped.Add(node.val);
+        //    Dfs(node.right);
+        //    Dfs(node.left);
+        //}
+        //else
+        //{
+        //    Dfs(node.left);
+        //    Dfs(node.right);
+        //}
     }
 }
+/*
+方法：深度优先搜索
+思路
+
+当做先序遍历的时候，我们可能会翻转某一个节点，尝试使我们当前的遍历序列与给定的行程序列相匹配。
+
+如果我们希望先序遍历序列的下一个数字是 voyage[i] ，那么至多只有一种可行的遍历路径供我们选择，因为所有节点的值都不相同。
+
+算法
+
+进行深度优先遍历。如果遍历到某一个节点的时候，节点值不能与行程序列匹配，那么答案一定是 [-1]。
+
+否则，当行程序列中的下一个期望数字 voyage[i] 与我们即将遍历的子节点的值不同的时候，我们就要翻转一下当前这个节点。
+
+JavaPython
+class Solution {
+    List<Integer> flipped;
+    int index;
+    int[] voyage;
+
+    public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
+        flipped = new ArrayList();
+        index = 0;
+        this.voyage = voyage;
+
+        dfs(root);
+        if (!flipped.isEmpty() && flipped.get(0) == -1) {
+            flipped.clear();
+            flipped.add(-1);
+        }
+
+        return flipped;
+    }
+
+    public void dfs(TreeNode node) {
+        if (node != null) {
+            if (node.val != voyage[index++]) {
+                flipped.clear();
+                flipped.add(-1);
+                return;
+            }
+
+            if (index < voyage.length && node.left != null &&
+                    node.left.val != voyage[index]) {
+                flipped.add(node.val);
+                dfs(node.right);
+                dfs(node.left);
+            } else {
+                dfs(node.left);
+                dfs(node.right);
+            }
+        }
+    }
+}
+复杂度分析
+
+时间复杂度：O(N)O(N)，其中 NN 是给定树中节点的数量。
+
+空间复杂度：O(N)O(N)。
+ 
+*/
