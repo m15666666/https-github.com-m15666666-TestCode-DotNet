76dbb5f001ca06b06717c088c27ebef9642d08dc
m15666666 <m15666666@126.com>
2019-12-20 08:34:24 +0800

    973. 最接近原点的 K 个点

---
+++ leetcode-cn/TestLeetCode/SubarraySumsDivisibleByKSolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+给定一个整数数组 A，返回其中元素之和可被 K 整除的（连续、非空）子数组的数目。
+
+ 
+
+示例：
+
+输入：A = [4,5,0,-2,-3,1], K = 5
+输出：7
+解释：
+有 7 个子数组满足其元素之和可被 K = 5 整除：
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+ 
+
+提示：
+
+1 <= A.length <= 30000
+-10000 <= A[i] <= 10000
+2 <= K <= 10000
+*/
+/// <summary>
+/// https://leetcode-cn.com/problems/subarray-sums-divisible-by-k/
+/// 974. 和可被 K 整除的子数组
+/// 
+/// </summary>
+class SubarraySumsDivisibleByKSolution
+{
+    public void Test()
+    {
+        //int[] nums = new int[] {3, 2, 4};
+        //int k = 6;
+        //var ret = LevelOrder((int[]) nums.Clone(), k);
+
+        //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
+    }
+
+    public int SubarraysDivByK(int[] A, int K)
+    {
+
+    }
+}
