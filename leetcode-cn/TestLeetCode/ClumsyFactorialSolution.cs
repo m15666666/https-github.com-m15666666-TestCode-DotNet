77b0b92f8895ceabb1bca55b27a7aeac2cf22a9c
m15666666 <m15666666@126.com>
2020-01-13 15:29:26 +0800

    1006. 笨阶乘

--- leetcode-cn/TestLeetCode/ClumsyFactorialSolution.cs
+++ leetcode-cn/TestLeetCode/ClumsyFactorialSolution.cs
@@ -36,7 +36,7 @@ using System.Text;
 */
 /// <summary>
 /// https://leetcode-cn.com/problems/clumsy-factorial/
-/// 
+/// 1006. 笨阶乘
 /// 
 /// </summary>
 class ClumsyFactorialSolution
@@ -52,6 +52,165 @@ class ClumsyFactorialSolution
 
     public int Clumsy(int N)
     {
+        switch (N)
+        {
+            case 1: return 1;
+            case 2: return 2;
+            case 3: return 6;
+            case 4: return 7;
+        }
+        
+        switch((N - 5 ) % 4)
+        {
+            case 0: return N + 2;
+            case 1: return N + 2;
+            case 2: return N - 1;
 
+            default:
+            case 3: return N + 1;
+        }
     }
 }
+/*
+4ms python
+vxiash
+发布于 2 个月前
+178 阅读
+```python
+class Solution(object):
+    def clumsy(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        if N <= 4:
+            return [1, 2, 6, 7][N-1]
+        mod = N & 3
+        return N + [1, 2, 2, -1][mod]
+
+(k+2)(k+1)/k
+= (k^2+3k+2)/k
+= k+3 + 2/k
+于是k>=3时，2/k项变0抹掉，原式就等于k+3
+
+而+(k+3) - (k+2)(k+1)/k就等于0，于是中间项全部约掉，只剩首尾需要处理
+
+例如：N=101
+clumsy(101)
+=101x100/99 + 98 - 97x96/95 + 94 - 93x92/91 + ... + 6 - 5x4/3 + 2 - 1
+=(99+3) + (98-98) + (94-94) + ... + (6-6) + 2 - 1
+=99+3+2-1=103
+=N+2
+
+类似可得，从N=5开始，结果在[N+2, N+2, N-1, N+1]中间循环
+
+再手算N<=4时的[1,2,6,7]，返回即可。
+
+    (k+2)(k+1)/k = (k^2 + 3k + 2)/k = (k+3) + 2/k
+    
+    1 = 1
+    5*4/3+2-1 = (3+3) + 2 - 1 = 7 = N+2
+    9*8/7+6-5*4/3+2-1 = (7+3) + 6 - (3+3) + 2 - 1 = (7+3) + 2 - 1 = 11 = N+2
+    
+    2*1 = 2
+    6*5/4+3-2*1 = (4+3) + 3 - 2 = 8 = N+2
+    10*9/8+7-6*5/4+3-2*1 = (8+3) + 7 - (4+3) + 3 - 2 = (8+3) + 3 - 2 = 12 = N+2
+    
+    3*2/1 = 6
+    7*6/5+4-3*2/1 = (5+3) + 4 - 6 = 6 = N-1
+    11*10/9+8-7*6/5+4-3*2/1 = (9+3) + 8 - (5+3) + 4 - 6 = (9+3) + 4 - 6 = 10 = N-1
+    
+    4*3/2+1 = (2+3+1) + 1 = 7
+    8*7/6+5-4*3/2+1 = (6+3) + 5 - (2+3+1) + 1 = 9 = N+1
+	
+ public class Solution
+    {
+        public int Clumsy(int N)
+        {
+            Queue<string> q = new Queue<string>();
+            q.Enqueue("+");
+            q.Enqueue("-");
+            int res = 0;
+            if (N > 2)
+            {
+                res = N * (N - 1) / (N - 2);
+                N -= 3;
+             
+                while (N > 0)
+                {
+                    var opr = q.Dequeue();
+                    if (opr == "+")
+                    {
+                        res += N;
+                        N--;
+                        q.Enqueue(opr);
+                        continue;
+                    }
+                    if (opr == "-")
+                    {
+                        if (N > 2)
+                        {
+                            res -= N * (N - 1) / (N - 2);
+                            N -= 3;
+                        }
+                        else {
+                            res -= N;
+                            return res;
+                        }
+                        q.Enqueue(opr);
+                        continue;
+                    }
+            
+                }
+            }
+     
+            else {
+                return N;
+            }
+            return res;
+        }
+
+    }
+
+public class Solution {
+    
+    public int Clumsy(int N) {
+        int SUM = -1;
+        int addtion = 0;
+        for(int i = N; i > 0; i -= 4){
+            int g = 0;
+            
+            for(int j = 0; j < 4; ++j){
+                int tmp = i - j;
+                if(tmp <= 0)break;
+                switch(j){
+                    case 0: {
+                        g = tmp;
+                    }break;
+                    case 1: {
+                        g *= tmp;
+                    }break;
+                    case 2: {
+                        g /= tmp;
+                    }break;
+                    case 3: {
+                        addtion += tmp;
+                    }break;
+                }
+            }
+            
+            //Debug
+            //if(i == 6)return addtion;
+            
+            if(SUM == -1){
+                SUM = g;
+            }else{
+                SUM -= g;
+            }
+        }
+        SUM += addtion;
+        //return addtion;
+        return SUM;
+    }
+}
+*/
