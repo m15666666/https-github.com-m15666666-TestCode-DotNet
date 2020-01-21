d383ff9500be7bb6939f32d93a7c7a58b85104e2
m15666666 <m15666666@126.com>
2020-01-21 09:56:49 +0800

    1017. 负二进制转换

--- leetcode-cn/TestLeetCode/ConvertToBase2Solution.cs
+++ leetcode-cn/TestLeetCode/ConvertToBase2Solution.cs
@@ -50,6 +50,54 @@ class ConvertToBase2Solution
 
     public string BaseNeg2(int N)
     {
+        if (N == 0) return "0";
+        var nums = baseK(N, -2);
+        StringBuilder ret = new StringBuilder();
+        foreach (var x in nums) ret.Append(x);
+        return ret.ToString();
+    }
 
+    private static IList<int> baseK(int N, int K)
+    {
+        if (N == 0) return new int[0];
+        List<int> ret = new List<int>();
+        while (N != 0)
+        {
+            int r = ((N % K) + Math.Abs(K)) % Math.Abs(K);
+            ret.Add(r);
+            N -= r;
+            N /= K;
+        }
+        ret.Reverse();
+        return ret;
     }
 }
+/*
+C++ 正负K进制转换通用题解
+大力王
+322 阅读
+通过数学推导可以得到+K/-K进制的通用转化法
+
+class Solution {
+public:
+    // 无论K是正数还是负数都支持（只支持-10～10进制，因为更高进制需要引入字母）
+    vector<int> baseK(int N, int K) {
+        if (N == 0) return {0};
+        vector<int> res;
+        while (N != 0) {
+            int r = ((N % K) + abs(K)) % abs(K); // 此处为关键
+            res.push_back(r);
+            N -= r;
+            N /= K;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+    string baseNeg2(int N) {
+        vector<int> nums = baseK(N, -2);
+        string res;
+        for (auto x : nums) res += to_string(x);
+        return res;
+    }
+}; 
+*/
