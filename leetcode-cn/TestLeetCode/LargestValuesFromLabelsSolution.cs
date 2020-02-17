94703ca4bab6f1dfe4d789b20abaf66baaba112a
chenliang5572 <liang.chen@moons.com.cn>
2020-02-17 20:42:15 +0800

    1090. 受标签影响的最大值

--- leetcode-cn/TestLeetCode/LargestValuesFromLabelsSolution.cs
+++ leetcode-cn/TestLeetCode/LargestValuesFromLabelsSolution.cs
@@ -61,6 +61,124 @@ class LargestValuesFromLabelsSolution
 
     public int LargestValsFromLabels(int[] values, int[] labels, int num_wanted, int use_limit)
     {
-        return default;
+        Term[] datas = new Term[values.Length];
+        int maxLabel = 0;
+        for (int i = 0; i < values.Length; i++)
+        {
+            var label = labels[i]; ;
+            datas[i].value = values[i];
+            datas[i].label = label;
+            if (maxLabel < label) maxLabel = label;
+        }
+
+        Array.Sort(datas, (i1, i2) => i2.value.CompareTo( i1.value));
+
+        int[] labelCounts = new int[maxLabel+1];
+        Array.Fill(labelCounts, 0);
+
+        int number = 0;
+        int sum = 0;
+        foreach (var data in datas)
+        {
+            int label = data.label;
+            if (labelCounts[label] < use_limit)
+            {
+                labelCounts[label]++;
+
+                number++;
+                sum += data.value;
+                if (num_wanted <= number) return sum;
+            }
+        }
+        return sum;
+    }
+
+    public struct Term
+    {
+        public int value;
+        public int label;
     }
 }
+/*
+贪心 C++
+ExecutionHY
+发布于 4 个月前
+342 阅读
+image.png
+
+贪心，优先取大的数字，且同一个标签最多取 use_limit 个，取满 num_wanted 个数为止。
+
+class Solution {
+public:
+    struct term {
+        int value, label;
+    };
+    static bool cmp(term a, term b) {
+        return a.value > b.value;
+    }
+    int largestValsFromLabels(vector<int>& values, vector<int>& labels, int num_wanted, int use_limit) {
+        vector<term> data;
+        data.resize(values.size());
+        int ml = 0;
+        for (int i = 0; i < values.size(); i++) {
+            data[i].value = values[i];
+            data[i].label = labels[i];
+            ml = max(ml, labels[i]);
+        }
+        sort(data.begin(), data.end(), cmp);
+
+        vector<int> bucket;
+        bucket.resize(ml+1);
+        int num = 0;
+        int sum = 0;
+        for (int i = 0; i < data.size(); i++) {
+            int lab = data[i].label;
+            if (bucket[lab] < use_limit) {
+                num++;
+                bucket[lab]++;
+                sum += data[i].value;
+                if (num >= num_wanted) return sum;
+            }
+        }
+        return sum;
+    }
+};
+下一篇：贪心算法，调用堆排序中的前k大排序
+
+public class Solution {
+    public int LargestValsFromLabels(int[] values, int[] labels, int num_wanted, int use_limit) {
+        //节点按照value降序，满足要求时 加入结果，纸质循环结束
+        if(num_wanted == 0 || use_limit == 0)
+            return 0;
+        
+        int len = values.Length;
+        int[][] pairs = new int[len][];
+        for(int i = 0; i < len; i++){
+            pairs[i] = new int[2];
+            pairs[i][0] = labels[i];
+            pairs[i][1] = values[i];
+        }
+        
+        var list = ( from pair in pairs
+                     orderby pair[1] descending
+                     select pair).ToList();
+        
+        // 记录当前label选择的数量 max{label} = 20000
+        int[] numLabel = new int[20001];  
+        int ans = 0;
+        // 记录选择的总数 |S|
+        int cnt = 0;
+        for(int i = 0; i < len; i++){
+            int lab = list[i][0];
+            if(numLabel[lab] >= use_limit)
+                continue;
+            ans += list[i][1];
+            numLabel[lab]++;
+            cnt++;
+            if(cnt >= num_wanted)
+                return ans;
+        }
+        return ans;
+    }
+} 
+*/
