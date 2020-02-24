abbe49de71a9ec532b3b679e9507ac80c2303d58
chenliang5572 <liang.chen@moons.com.cn>
2020-02-24 12:04:09 +0800

    1109. 航班预订统计

--- leetcode-cn/TestLeetCode/Program.cs
+++ leetcode-cn/TestLeetCode/Program.cs
@@ -10,7 +10,7 @@ namespace TestLeetCode
         static void Main(string[] args)
         {
             // Test.CSharpClass.Bitset.Test();
-            var classname = $"{StringUtils.Capitalize("corporate-flight-bookings", "-")}Solution";
+            var classname = $"{StringUtils.Capitalize("delete-nodes-and-return-forest", "-")}Solution";
             Debug.WriteLine(classname);
 
             new FillingBookcaseShelvesSolution().Test();
