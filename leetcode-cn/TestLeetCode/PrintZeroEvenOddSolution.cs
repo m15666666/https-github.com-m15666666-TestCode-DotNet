a91eaa5f44d5bc101dc18c72e216f4deb5e07b5a
chenliang5572 <liang.chen@moons.com.cn>
2020-02-28 11:48:14 +0800

    1116. 打印零与奇偶数

--- leetcode-cn/TestLeetCode/PrintZeroEvenOddSolution.cs
+++ leetcode-cn/TestLeetCode/PrintZeroEvenOddSolution.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 
 /*
@@ -55,19 +56,348 @@ class PrintZeroEvenOddSolution
         this.n = n;
     }
 
+    private readonly System.Threading.SemaphoreSlim _zero = new System.Threading.SemaphoreSlim(1);
+    private readonly System.Threading.SemaphoreSlim _even = new System.Threading.SemaphoreSlim(0);
+    private readonly System.Threading.SemaphoreSlim _odd = new System.Threading.SemaphoreSlim(0);
+
     // printNumber(x) outputs "x", where x is an integer.
     public void Zero(Action<int> printNumber)
     {
-
+        //SpinWait
+        for (int i = 0; i < n; i++)
+        {
+            _zero.Wait();
+            printNumber(0);
+            if ((i & 1) == 0) _odd.Release();
+            else _even.Release();
+        }
     }
 
     public void Even(Action<int> printNumber)
     {
-
+        for (int i = 2; i <= n; i += 2)
+        {
+            _even.Wait();
+            printNumber(i);
+            _zero.Release();
+        }
     }
 
     public void Odd(Action<int> printNumber)
     {
-
+        for (int i = 1; i <= n; i += 2)
+        {
+            _odd.Wait();
+            printNumber(i);
+            _zero.Release();
+        }
     }
 }
+/*
+
+JAVA并发工具类大练兵
+KevinBauer
+发布于 5 个月前
+3.9k 阅读
+原文地址： https://zhuanlan.zhihu.com/p/81626432
+
+方案一：Semaphore
+借助信号量来建立屏障，还是很方便的：
+
+class ZeroEvenOdd {
+    private int n;
+    
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    Semaphore z = new Semaphore(1);
+    Semaphore e = new Semaphore(0);
+    Semaphore o = new Semaphore(0);
+	
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for(int i=0; i<n;i++) {
+        	z.acquire();
+        	printNumber.accept(0);
+        	if((i&1)==0) {
+        		o.release();
+        	}else {
+        		e.release();
+        	}
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for(int i=2; i<=n; i+=2) {
+        	e.acquire();
+        	printNumber.accept(i);
+        	z.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for(int i=1; i<=n; i+=2) {
+        	o.acquire();
+        	printNumber.accept(i);
+        	z.release();
+        }
+    }
+}
+方案二：Lock
+之前听课时听过老师讲：“凡是可以用信号量解决的问题，都可以用管程模型来解决”，那么我们试试吧（实践发现，也确实可以，但逻辑有点绕不够直观）：
+
+class ZeroEvenOdd {
+    private int n;
+    
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    Lock lock = new ReentrantLock();
+    Condition z = lock.newCondition();
+    Condition num = lock.newCondition();
+    volatile boolean zTurn = true;
+    volatile int zIndex = 0;
+	
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for(;zIndex<n;) {
+            lock.lock();
+            try {
+        	while(!zTurn) {
+        		z.await();
+        	}
+        	printNumber.accept(0);
+        	zTurn = false;
+        	num.signalAll();
+                zIndex++;
+            }finally {
+        	lock.unlock();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for(int i=2; i<=n; i+=2) {
+            lock.lock();
+            try {
+        	while(zTurn || (zIndex&1)==1) {
+        		num.await();
+        	}
+        	printNumber.accept(i);
+        	zTurn = true;
+        	z.signal();
+            }finally {
+        	lock.unlock();
+            }
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for(int i=1; i<=n; i+=2) {
+            lock.lock();
+            try {
+        	while(zTurn || (zIndex&1)==0) {
+        		num.await();
+        	}
+        	printNumber.accept(i);
+        	zTurn = true;
+        	z.signal();
+            }finally {
+        	lock.unlock();
+            }
+        }
+    }
+}
+方案三：无锁
+老规矩，但凡用了锁的，都来试试可否变成无锁的（本机测试是可行的，但测评平台报超时）：
+
+class ZeroEvenOdd {
+    private int n;
+    
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    volatile int stage = 0;
+	
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for(int i=0;i<n;i++) {
+        	while(stage>0);
+    		printNumber.accept(0);
+    		if((i&1)==0) {
+    			stage = 1;
+    		}else {
+    			stage = 2;
+    		}
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for(int i=2; i<=n; i+=2) {
+        	while(stage!=2);
+    		printNumber.accept(i);
+    		stage = 0;
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for(int i=1; i<=n; i+=2) {
+        	while(stage!=1);
+    		printNumber.accept(i);
+    		stage = 0;
+        }
+    }
+}
+下一篇：用Lock和Condition解决问题
+
+using System.Threading;
+public class ZeroEvenOdd {
+    private int n;
+    private SpinWait _spinWait=new SpinWait();
+    private bool _state=true;
+    private bool _end=false;
+    private int _state2=0;
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    public void Zero(Action<int> printNumber) {
+        while(!_end){
+        SpinWait.SpinUntil(()=>_state);
+        if(_state2==n) {
+        _end=true;
+        _state=false;
+            return;}
+        printNumber(0);
+         ++_state2;
+        _state=false;}
+    }
+
+    public void Even(Action<int> printNumber) {
+        while(!_end){
+        SpinWait.SpinUntil(()=>!_state&&_state2%2==0);
+         if(_end){
+         ++_state2;
+            return;
+         }
+        printNumber(_state2);
+        _state=true;
+        }
+    }
+
+    public void Odd(Action<int> printNumber) {
+        while(!_end){
+        SpinWait.SpinUntil(()=>!_state&&_state2%2==1);
+         if(_end){
+         ++_state2;
+            return;
+         }
+        printNumber(_state2);
+        _state=true;
+        }
+    }
+}
+
+using System.Threading;
+public class ZeroEvenOdd
+{
+	private int n;
+	private Semaphore zero = new Semaphore(1, 1);
+	private Semaphore even = new Semaphore(0, 1);
+	private Semaphore odd = new Semaphore(0, 1);
+	public ZeroEvenOdd(int n)
+	{
+		this.n = n;
+	}
+
+	//输出0
+	// printNumber(x) outputs "x", where x is an integer.
+	public void Zero(Action<int> printNumber)
+	{
+		for (var i = 0; i < n; i++)
+		{
+			zero.WaitOne();
+			printNumber(0);
+			lock (this)
+			{
+				if (i % 2 == 1)
+				{ even.Release(1); }
+				if (i % 2 == 0)
+				{ odd.Release(1); }
+			}
+		}
+	}
+	//输出偶数
+	public void Even(Action<int> printNumber)
+	{
+		for (var i = 2; i <= n; i = i + 2)
+		{
+			even.WaitOne();
+			printNumber(i);
+			lock (this)
+			{
+				zero.Release(1);
+			}
+		}
+	}
+
+	//输出奇数
+	public void Odd(Action<int> printNumber)
+	{
+		for (var i = 1; i <= n; i = i + 2)
+		{
+			odd.WaitOne();
+			printNumber(i);
+			lock (this)
+			{
+				zero.Release(1);
+			}
+		}
+	}
+}
+	
+using System.Threading;
+public class ZeroEvenOdd {
+    private int n;
+    private static   AutoResetEvent oddResetEvent = new AutoResetEvent(false);
+    private static  AutoResetEvent evenResetEvent = new AutoResetEvent(false);
+    private static   AutoResetEvent zeroResetEvent = new AutoResetEvent(true);
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    public void Zero(Action<int> printNumber) {
+        int i=0;
+        while(this.n>i){
+            zeroResetEvent.WaitOne();
+            printNumber(0);
+            if(i%2==0){
+                evenResetEvent.Set();
+            }else
+                oddResetEvent.Set();
+            i++;
+        }
+    }
+
+    public void Even(Action<int> printNumber) {
+        for(int i=2;i<=this.n;i+=2){
+            oddResetEvent.WaitOne();
+            printNumber(i);
+            zeroResetEvent.Set();
+        }
+    }
+
+    public void Odd(Action<int> printNumber) {
+        for(int i=1;i<=this.n;i+=2)
+        {
+            evenResetEvent.WaitOne();
+            printNumber(i);
+            zeroResetEvent.Set();
+        }
+    }
+}
+
+ 
+*/
