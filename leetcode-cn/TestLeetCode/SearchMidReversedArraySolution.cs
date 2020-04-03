7d3e833cbabc2552e8335c2a94bfd30ed8d18b44
m15666666 <m15666666@126.com>
2020-04-03 09:06:06 +0800

    33. 搜索旋转排序数组

--- leetcode-cn/TestLeetCode/SearchMidReversedArraySolution.cs
+++ leetcode-cn/TestLeetCode/SearchMidReversedArraySolution.cs
@@ -4,10 +4,30 @@ using System.Linq;
 using System.Text;
 
 
+/*
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+
+搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+
+你可以假设数组中不存在重复的元素。
+
+你的算法时间复杂度必须是 O(log n) 级别。
+
+示例 1:
+
+输入: nums = [4,5,6,7,0,1,2], target = 0
+输出: 4
+示例 2:
+
+输入: nums = [4,5,6,7,0,1,2], target = 3
+输出: -1
+*/
 /// <summary>
-/// https://leetcode-cn.com/problems/search-in-rotated-sorted-array/description/
-/// 搜索旋转排序数组
-/// 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+/// https://leetcode-cn.com/problems/search-in-rotated-sorted-array
+/// 33. 搜索旋转排序数组
+/// 
 /// </summary>
 class SearchMidReversedArraySolution
 {
@@ -100,3 +120,128 @@ class SearchMidReversedArraySolution
         return -1;
     }
 }
+/*
+
+搜索旋转排序数组
+力扣 (LeetCode)
+发布于 10 个月前
+59.5k
+
+方法：二分查找
+题目要找到一种 O(logN)O(logN) 时间内的搜索方法，这提示我们可以用二分查找的方法。
+
+算法非常直接：
+
+找到旋转的下标 rotation_index ，也就是数组中最小的元素。二分查找在这里可以派上用场。
+在选中的数组区域中再次使用二分查找。
+
+
+class Solution {
+  int [] nums;
+  int target;
+
+  public int find_rotate_index(int left, int right) {
+    if (nums[left] < nums[right])
+      return 0;
+
+    while (left <= right) {
+      int pivot = (left + right) / 2;
+      if (nums[pivot] > nums[pivot + 1])
+        return pivot + 1;
+      else {
+        if (nums[pivot] < nums[left])
+          right = pivot - 1;
+        else
+          left = pivot + 1;
+      }
+    }
+    return 0;
+  }
+
+  public int search(int left, int right) {
+    while (left <= right) {
+      int pivot = (left + right) / 2;
+      if (nums[pivot] == target)
+        return pivot;
+      else {
+        if (target<nums[pivot])
+          right = pivot - 1;
+        else
+          left = pivot + 1;
+      }
+    }
+    return -1;
+  }
+
+  public int search(int[] nums, int target)
+{
+    this.nums = nums;
+    this.target = target;
+
+    int n = nums.length;
+
+    if (n == 0)
+        return -1;
+    if (n == 1)
+        return this.nums[0] == target ? 0 : -1;
+
+    int rotate_index = find_rotate_index(0, n - 1);
+
+    // if target is the smallest element
+    if (nums[rotate_index] == target)
+        return rotate_index;
+    // if array is not rotated, search in the entire array
+    if (rotate_index == 0)
+        return search(0, n - 1);
+    if (target < nums[0])
+        // search in the right side
+        return search(rotate_index, n - 1);
+    // search in the left side
+    return search(0, rotate_index);
+}
+}
+复杂度分析
+
+时间复杂度： O(logN)O(logN) 。
+空间复杂度： O(1)O(1) 。
+
+public class Solution {
+     public int Search(int[] nums, int target)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return -1;
+            }
+            int left = 0;
+            int right = nums.Length - 1;
+            int mid;
+            while(left <= right)
+            {
+                mid  = left + (right - left) / 2;
+                if(nums[mid] == target)
+                {
+                    return mid;
+                }
+                if(nums[left] <= nums[mid])
+                {
+                    if(target >= nums[left] && target < nums[mid])
+                    {
+                        right = mid - 1;
+                    }else
+                    {
+                        left = mid + 1;
+                    }
+                }else{
+                    if(target > nums[mid] && target <= nums[right])
+                    {
+                        left = mid + 1;
+                    }else{
+                        right = mid - 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+}
+*/
