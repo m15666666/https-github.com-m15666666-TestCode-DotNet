b6fd07c1995ac112b49d2befc6ff5615d23e7872
m15666666 <m15666666@126.com>
2020-07-13 09:57:24 +0800

    133. 克隆图

--- leetcode-cn/TestLeetCode/CloneGraphSolution.cs
+++ leetcode-cn/TestLeetCode/CloneGraphSolution.cs
@@ -1,8 +1,61 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
+/*
+给你无向 连通 图中一个节点的引用，请你返回该图的 深拷贝（克隆）。
+
+图中的每个节点都包含它的值 val（int） 和其邻居的列表（list[Node]）。
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+
+测试用例格式：
+
+简单起见，每个节点的值都和它的索引相同。例如，第一个节点值为 1（val = 1），第二个节点值为 2（val = 2），以此类推。该图在测试用例中使用邻接列表表示。
+
+邻接列表 是用于表示有限图的无序列表的集合。每个列表都描述了图中节点的邻居集。
+
+给定节点将始终是图中的第一个节点（值为 1）。你必须将 给定节点的拷贝 作为对克隆图的引用返回。
+
+ 
+
+示例 1：
+
+输入：adjList = [[2,4],[1,3],[2,4],[1,3]]
+输出：[[2,4],[1,3],[2,4],[1,3]]
+解释：
+图中有 4 个节点。
+节点 1 的值是 1，它有两个邻居：节点 2 和 4 。
+节点 2 的值是 2，它有两个邻居：节点 1 和 3 。
+节点 3 的值是 3，它有两个邻居：节点 2 和 4 。
+节点 4 的值是 4，它有两个邻居：节点 1 和 3 。
+示例 2：
+
+输入：adjList = [[]]
+输出：[[]]
+解释：输入包含一个空列表。该图仅仅只有一个值为 1 的节点，它没有任何邻居。
+示例 3：
+
+输入：adjList = []
+输出：[]
+解释：这个图是空的，它不含任何节点。
+示例 4：
+
+输入：adjList = [[2],[1]]
+输出：[[2],[1]]
+ 
+
+提示：
+
+节点数不超过 100 。
+每个节点值 Node.val 都是唯一的，1 <= Node.val <= 100。
+无向图是一个简单图，这意味着图中没有重复的边，也没有自环。
+由于图是无向的，如果节点 p 是节点 q 的邻居，那么节点 q 也必须是节点 p 的邻居。
+图是连通图，你可以从给定节点访问到所有节点。
+
+*/
 
 /// <summary>
 /// https://leetcode-cn.com/problems/clone-graph/
@@ -11,7 +64,7 @@ using System.Text;
 /// https://www.cnblogs.com/immiao0319/p/8392052.html
 /// https://blog.csdn.net/Bendaai/article/details/80985328
 /// </summary>
-class CloneGraphSolution
+internal class CloneGraphSolution
 {
     public void Test()
     {
@@ -22,13 +75,54 @@ class CloneGraphSolution
         //Console.WriteLine(string.Join(",", ret.Select(v => v.ToString())));
     }
 
+    public class Node
+    {
+        public int val;
+        public IList<Node> neighbors;
+
+        public Node(int x)
+        {
+            val = x; neighbors = new List<Node>();
+        }
+    }
+
+    public Node CloneGraph(Node node)
+    {
+        if (node == null) return node;
+
+        Dictionary<Node, Node> visited = new Dictionary<Node, Node>();
+
+        var queue = new Queue<Node>();
+        queue.Enqueue(node);
+        var ret = new Node(node.val);
+        visited.Add(node, ret);
+
+        while (0 < queue.Count)
+        {
+            Node n = queue.Dequeue();
+            var copyNeighbors = visited[n].neighbors;
+            foreach (Node neighbor in n.neighbors)
+            {
+                Node copy;
+                if (!visited.ContainsKey(neighbor))
+                {
+                    copy = new Node(neighbor.val);
+                    visited.Add(neighbor, copy);
+                    queue.Enqueue(neighbor);
+                }
+                else copy = visited[neighbor];
+                copyNeighbors.Add(copy);
+            }
+        }
+        return ret;
+    }
 
     public UndirectedGraphNode CloneGraph(UndirectedGraphNode node)
     {
         if (node == null) return null;
 
-        Dictionary<UndirectedGraphNode, UndirectedGraphNode> map = Bfs( node );
-        foreach( var head in map.Keys )
+        Dictionary<UndirectedGraphNode, UndirectedGraphNode> map = Bfs(node);
+        foreach (var head in map.Keys)
         {
             var neighbors = map[head].neighbors;
             foreach (var neighbor in head.neighbors)
@@ -38,17 +132,17 @@ class CloneGraphSolution
         return map[node];
     }
 
-    private static Dictionary<UndirectedGraphNode, UndirectedGraphNode> Bfs( UndirectedGraphNode node )
+    private static Dictionary<UndirectedGraphNode, UndirectedGraphNode> Bfs(UndirectedGraphNode node)
     {
         Dictionary<UndirectedGraphNode, UndirectedGraphNode> ret = new Dictionary<UndirectedGraphNode, UndirectedGraphNode>();
         Queue<UndirectedGraphNode> queue = new Queue<UndirectedGraphNode>();
 
         queue.Enqueue(node);
         ret[node] = new UndirectedGraphNode(node.label);
-        while( 0 < queue.Count)
+        while (0 < queue.Count)
         {
             var head = queue.Dequeue();
-            foreach( var neighbor in head.neighbors )
+            foreach (var neighbor in head.neighbors)
             {
                 if (ret.ContainsKey(neighbor)) continue;
                 queue.Enqueue(neighbor);
@@ -58,15 +152,20 @@ class CloneGraphSolution
 
         return ret;
     }
-
 }
 
 /**
  * Definition for undirected graph.
- *  
+ *
  */
-public class UndirectedGraphNode {
+
+public class UndirectedGraphNode
+{
     public int label;
     public IList<UndirectedGraphNode> neighbors;
-    public UndirectedGraphNode(int x) { label = x; neighbors = new List<UndirectedGraphNode>(); }
+
+    public UndirectedGraphNode(int x)
+    {
+        label = x; neighbors = new List<UndirectedGraphNode>();
+    }
 }
