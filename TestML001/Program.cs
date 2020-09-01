52c3174668f5ce571d199e722e6e14d28d1ff969
m15666666 <m15666666@126.com>
2020-09-01 09:06:18 +0800

    207. 课程表

---
+++ TestML001/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace TestML001
+{
+    /// <summary>
+    /// https://dotnet.microsoft.com/learn/ml-dotnet/get-started-tutorial/create
+    /// https://dotnet.microsoft.com/learn/ml-dotnet/get-started-tutorial/next
+    /// 微软机器学习
+    /// 
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
