d5b7cb6761ec1cf5dea538b0f958db645a109adf
m15666666 <m15666666@126.com>
2020-09-02 09:24:10 +0800

    208. 实现 Trie (前缀树)

--- leetcode-cn/TestLeetCode/ImplementTriePrefixTreeSolution.cs
+++ leetcode-cn/TestLeetCode/ImplementTriePrefixTreeSolution.cs
@@ -1,22 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 
+/*
+实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+
+示例:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // 返回 true
+trie.search("app");     // 返回 false
+trie.startsWith("app"); // 返回 true
+trie.insert("app");   
+trie.search("app");     // 返回 true
+说明:
+
+你可以假设所有的输入都是由小写字母 a-z 构成的。
+保证所有输入均为非空字符串。
+
+*/
 /// <summary>
 /// https://leetcode-cn.com/problems/implement-trie-prefix-tree/
 /// https://leetcode.com/articles/implement-trie-prefix-tree/
 /// 208. 实现 Trie (前缀树)
-/// 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
-/// 示例:
-/// Trie trie = new Trie();
-/// trie.insert("apple");
-/// trie.search("apple");   // 返回 true
-/// trie.search("app");     // 返回 false
-/// trie.startsWith("app"); // 返回 true
-/// trie.insert("app");   
-/// trie.search("app");     // 返回 true
 /// https://segmentfault.com/a/1190000015804960
 /// </summary>
 class Trie // ImplementTriePrefixTreeSolution
