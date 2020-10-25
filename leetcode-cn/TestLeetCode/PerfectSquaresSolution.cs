c284e376f297ae62f8308082d88fe1a74e000847
m15666666 <m15666666@126.com>
2020-10-25 10:46:54 +0800

    282. 给表达式添加运算符

--- leetcode-cn/TestLeetCode/PerfectSquaresSolution.cs
+++ leetcode-cn/TestLeetCode/PerfectSquaresSolution.cs
@@ -502,6 +502,31 @@ class Solution {
     return 3;
   }
 }
+
+位运算很不错
+python
+class Solution:
+    def isSquare(self, n: int) -> bool:
+        sq = int(math.sqrt(n))
+        return sq*sq == n
+
+    def numSquares(self, n: int) -> int:
+        # four-square and three-square theorems
+        while (n & 3) == 0:
+            n >>= 2      # reducing the 4^k factor from number
+        if (n & 7) == 7: # mod 8
+            return 4
+
+        if self.isSquare(n):
+            return 1
+        # check if the number can be decomposed into sum of two squares
+        for i in range(1, int(n**(0.5)) + 1):
+            if self.isSquare(n - i*i):
+                return 2
+        # bottom case from the three-square theorem
+        return 3
+
+
 复杂度分析
 
 时间复杂度：\mathcal{O}(\sqrt{n})O( 
