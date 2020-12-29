6e82b687db8714f1b8ccf37548c718c240fd31a7
m15666666 <m15666666@126.com>
2020-12-29 14:13:28 +0800

    gcd

--- leetcode-cn/TestLeetCode/BasicMath/GcdLcm.cs
+++ leetcode-cn/TestLeetCode/BasicMath/GcdLcm.cs
@@ -4,6 +4,7 @@ namespace TestLeetCode.BasicMath
 {
     /// <summary>
     /// 最大公约数、最小公倍数
+    /// 参考：https://www.csharpstar.com/c-program-to-find-gcd-and-lcm/
     /// </summary>
     public static class GcdLcm
     {
@@ -11,6 +12,9 @@ namespace TestLeetCode.BasicMath
         {
             if (GCD(8, 4) != 4) throw new Exception();
             if (GCD(6, 4) != 2) throw new Exception();
+            if (GCD(-6, 4) != -2) throw new Exception();
+            if (GCD(-6, 6) != -6) throw new Exception();
+            if (GCD(6, -6) != -6) throw new Exception();
             if (LCM(8, 4) != 8) throw new Exception();
             if (LCM(6, 4) != 12) throw new Exception();
         }
@@ -26,7 +30,22 @@ namespace TestLeetCode.BasicMath
             if (num1 == 0 && num2 == 0) return 1;
             if (num1 == 0) return num2;
             if (num2 == 0) return num1;
-            return GCD(num2, num1 % num2);
+            if (num1 < num2)
+            {
+                int t = num1;
+                num1 = num2;
+                num2 = t;
+            }
+
+            int mod;
+            while (num2 != 0)
+            {
+                mod = num1 % num2;
+                num1 = num2;
+                num2 = mod;
+            }
+            return num1;
+            //return GCD(num2, num1 % num2);
         }
 
         /// <summary>
