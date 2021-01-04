44c0b7de41733dbb7db68465c191908e69642c6a
m15666666 <m15666666@126.com>
2021-01-04 15:36:07 +0800

    WebSocketDocuments

---
+++ WebSocketDocuments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebSocketDocuments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
